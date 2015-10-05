37c8588f92eadc76669f5638ad7e1b5cab6216d9
Filip Noetzel <filip@j03.de>
2015-10-05 14:55:46 +0200

    Make flake8 happy

--- gather2-core/core/tests.py
+++ gather2-core/core/tests.py
@@ -1,9 +1,9 @@
+import pytest
+
 from core.models import Survey
 
-import pytest
 
 @pytest.mark.django_db
 def test_my_user():
     a = Survey(name="Hello World")
     assert str(a) == "None - Hello World"
-
