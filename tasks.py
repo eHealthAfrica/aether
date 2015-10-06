44d5d0e6ab59d5767ef770ce0989e1ade98ce9d8
Filip Noetzel <filip@j03.de>
2015-10-06 14:11:07 +0200

    Add check/fix invoke commands

--- tasks.py
+++ tasks.py
@@ -17,3 +17,10 @@ def test(configuration="Test"):
         configuration
     ))
 
+@task
+def check():
+    run("flake8 --exclude migrations --ignore E501,C901 ./gather2-core")
+
+@task
+def fix():
+    run("autopep8 -r --in-place --exclude migrations --max-line-length 120 ./gather2-core")
