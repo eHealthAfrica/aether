84d3f6cc7c2c880a02b9d32d66b08d2de6641902
Filip Noetzel <filip@j03.de>
2015-10-30 17:12:21 +0100

    Use pytest.ini to configure DJANGO_CONFIGURATION

--- tasks.py
+++ tasks.py
@@ -26,7 +26,7 @@ def runserver(configuration="Dev"):
 
 @task
 def test(configuration="Test"):
-    _run("DJANGO_CONFIGURATION={} py.test gather2*/tests.py".format(
+    _run("py.test gather2*/tests.py".format(
         configuration
     ))
 
