be57fd30b27b67737c3e3670ebeb6d2bddb8920d
MrPink <will.pink@gmail.com>
2017-01-06 12:21:26 +0100

    typo in the script and removing the unrequired var

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,8 +13,6 @@ TAG="${TRAVIS_TAG}"
 COMMIT="${TRAVIS_COMMIT}"
 BRANCH="${TRAVIS_BRANCH}"
 PR="${TRAVIS_PULL_REQUEST}"
-DOCKER_IMAGE_REPO="${DOCKER_IMAGE_REPO}"
-
 
 if ! [ -n "${TAG}" ]; then
   echo "Not a git tag, tagging as: ${COMMIT}"
@@ -22,10 +20,14 @@ if ! [ -n "${TAG}" ]; then
 fi
 export TAG
 
+git clone "https://${GH_USER}:${GH_TOKEN}@github.com/eHealthAfrica/beanstalk-deploy" "${script_dir}/.ebextensions"
+
 $(aws ecr get-login --region="${AWS_REGION}")
 for APP in $APPS
 do
+	echo "Tagging "${DOCKER_IMAGE_REPO}/${APP}:${TAG}"
   docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}/${APP}:${TAG}"
+  echo "Pushsing to ${DOCKER_IMAGE_REPO}/${APP}:${TAG}"
   docker push "${DOCKER_IMAGE_REPO}/${APP}:${TAG}"
 
   tmp_dir="tmp"
@@ -35,7 +37,7 @@ do
   pushd "${script_dir}" >/dev/null
   zip_file="${tmp_dir}/deploy.zip"
   zip -r "${zip_file}" ".ebextensions" -x '*.git*'
-  zip -j "${zip_file}" "${tmp_dir}"/* -x "${zip_file}
+  zip -j "${zip_file}" "${tmp_dir}"/* -x "${zip_file}"
 
   eb deploy "${APP}-dev" -l "${TAG}"
 done
