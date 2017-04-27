1ba26be76f10cdf416e7c24cd71fb4dcd3787aec
William Pink <will.pink@gmail.com>
2017-04-27 14:16:49 +0200

    Conf/will/deployment stuff (#74)
    * fix deploy
    
    * push develop too

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,8 @@ TAG="${TRAVIS_TAG}"
 COMMIT="${TRAVIS_COMMIT}"
 BRANCH="${TRAVIS_BRANCH}"
 PR="${TRAVIS_PULL_REQUEST}"
+AWS_DEFAULT_REGION="eu-west-1"
+DOCKER_IMAGE_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
 
 if ! [ -n "${TAG}" ]; then
   echo "Not a git tag, tagging as: ${COMMIT}"
@@ -21,11 +23,13 @@ export TAG
 $(aws ecr get-login --region="${AWS_REGION}")
 for APP in "${APPS[@]}"
 do
-	echo "Tagging "${DOCKER_IMAGE_REPO}-${ENV}/${APP}:${TAG}"
-  docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}-${ENV}/${APP}:${TAG}"
-  echo "Pushing to ${DOCKER_IMAGE_REPO}-${ENV}/${APP}:${TAG}"
-  docker push "${DOCKER_IMAGE_REPO}-${ENV}/${APP}:${TAG}"
+	echo "Tagging "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
+  echo "Pushing to ${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker push "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker push "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
 
   echo "Deploying ${APP}-${ENV}-${TAG}"
-  ecs deploy gather2-${ENV} ${APP} -i ${DOCKER_IMAGE_REPO}-${ENV}/${APP}:${TAG}" --timeout 300
+  ecs deploy gather2-${ENV} ${APP} -i ${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}" --timeout 300
 done
