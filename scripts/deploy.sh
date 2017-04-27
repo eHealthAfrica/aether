c257e5fa24ce1ab7c75891a6f2f6920a9fc0a165
William Pink <will.pink@gmail.com>
2017-04-27 16:55:21 +0200

    More deployment fixes (#75)
    * fix deploy
    
    * push develop too
    
    * increasing timeout
    
    * DOCKER_IMAGE_URL is getting picked up

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ COMMIT="${TRAVIS_COMMIT}"
 BRANCH="${TRAVIS_BRANCH}"
 PR="${TRAVIS_PULL_REQUEST}"
 AWS_DEFAULT_REGION="eu-west-1"
-DOCKER_IMAGE_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
+IMAGE_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
 
 if ! [ -n "${TAG}" ]; then
   echo "Not a git tag, tagging as: ${COMMIT}"
@@ -23,13 +23,14 @@ export TAG
 $(aws ecr get-login --region="${AWS_REGION}")
 for APP in "${APPS[@]}"
 do
-	echo "Tagging "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
-  docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
-  docker tag "${APP}:latest" "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
-  echo "Pushing to ${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
-  docker push "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}"
-  docker push "${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
+	echo "Tagging "${IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker tag "${APP}:latest" "${IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker tag "${APP}:latest" "${IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
+  echo "Pushing to ${IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker push "${IMAGE_REPO}/${APP}-${ENV}:${TAG}"
+  docker push "${IMAGE_REPO}/${APP}-${ENV}:${BRANCH}"
 
   echo "Deploying ${APP}-${ENV}-${TAG}"
-  ecs deploy gather2-${ENV} ${APP} -i ${DOCKER_IMAGE_REPO}/${APP}-${ENV}:${TAG}" --timeout 300
+  ecs deploy gather2-${ENV} ${APP} -i ${IMAGE_REPO}/${APP}-${ENV}:${TAG}" --timeout 600
+
 done
