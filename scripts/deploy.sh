999ab5ed7f8a2367e1a16bac785352c822f2640c
William Pink <will.pink@gmail.com>
2017-06-20 13:22:31 +0200

    chore: continuous deployment for Gather2 production (#104)
    * chore: continuous deployment for Gather2 prod
    
    * updating message

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,38 +3,28 @@ set -e
 
 export APPS=( core couchdb-sync odk-importer )
 
-if [ "${TRAVIS_BRANCH}" == "develop" ]; then
-  export ENV="dev"
-fi
-
-
-TAG="${TRAVIS_TAG}"
 COMMIT="${TRAVIS_COMMIT}"
 BRANCH="${TRAVIS_BRANCH}"
-PR="${TRAVIS_PULL_REQUEST}"
 export AWS_DEFAULT_REGION="eu-west-1"
 IMAGE_REPO="387526361725.dkr.ecr.eu-west-1.amazonaws.com"
 
-if ! [ -n "${TAG}" ]; then
-  echo "Not a git tag, tagging as: ${COMMIT}"
-  TAG="${COMMIT}"
+if [ "${BRANCH}" == "develop" ]; then
+  export ENV="dev"
 elif [ "${BRANCH}" == "master" ]; then
-  echo "tagged commit on master, setting ENV to production"
-  export ENV="prod" 
+  echo "commit on master, setting ENV to production"
+  export ENV="prod"
 fi
-
-export TAG
 
 $(aws ecr get-login --region="${AWS_DEFAULT_REGION}")
 for APP in "${APPS[@]}"
 do
   GATHER2_APP="gather2-${APP}"
-  echo "Building Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${TAG}"
+  echo "Building Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
   docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
-  docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${TAG}"
-  echo "Pushing Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${TAG}"
-  docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${TAG}"
+  docker tag $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}"
+  echo "Pushing Docker image ${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
   docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${BRANCH}"
+  docker push "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}"
   echo "Deploying ${APP} to ${ENV}"
-  ecs deploy --timeout 600 "gather2-${ENV}" $GATHER2_APP -i $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${TAG}"
+  ecs deploy --timeout 600 "gather2-${ENV}" $GATHER2_APP -i $APP "${IMAGE_REPO}/${GATHER2_APP}-${ENV}:${COMMIT}"
 done
