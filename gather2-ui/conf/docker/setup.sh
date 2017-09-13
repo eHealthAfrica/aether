b22ba372d6b0e1c47db097c7a405e623446cbad7
Obdulia Losantos <obdulia.losantos@gmail.com>
2017-09-13 11:23:55 +0200

    chore: upgrade dependencies (#172)
    * chore: add more options to .gitignore
    
    * chore(ui):  upgrade npm dependencies
    
    * chore: upgrade pip dependencies
    
    * chore(ui): upgrade node@6.11.3
    
    * chore(ui): json-loader is not longer needed

--- gather2-ui/conf/docker/setup.sh
+++ gather2-ui/conf/docker/setup.sh
@@ -12,7 +12,7 @@ export PYTHONUNBUFFERED=true
 
 # https://nodejs.org
 export NPM_CONFIG_LOGLEVEL=info
-export NODE_VERSION=6.11.0
+export NODE_VERSION=6.11.3
 
 
 ################################################################################
