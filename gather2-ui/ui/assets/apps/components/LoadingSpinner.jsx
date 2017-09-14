188f622629b35490806b01494df24ee92af851d4
Sabine Claassen <been@b33n.net>
2017-09-14 16:42:46 +0200

    Css fixes (#179)
    * Styled spinner
    
    * responsive columns
    
    * Fixed height of edit container
    
    * align record numbers in cards
    
    * Improved data views
    
    * removed unused css
    
    * Removed webkit prefix
    
    * use variable for navbar height

--- gather2-ui/ui/assets/apps/components/LoadingSpinner.jsx
+++ gather2-ui/ui/assets/apps/components/LoadingSpinner.jsx
@@ -6,7 +6,7 @@ export default class LoadingSpinner extends Component {
     return (
       <div data-qa='data-loading' className='container-fluid'>
         <p className='alert alert-info'>
-          <i className='loading-spinner mr-1' />
+          <i className='loading-spinner mr-2' />
           <FormattedMessage
             id='alert.loading'
             defaultMessage='Loading data from server…' />
