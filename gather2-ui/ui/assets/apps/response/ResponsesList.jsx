3a9cd5fed4fa33cbe01e5149b7ed9ea4ab8b4c94
Obdulia Losantos <obdulia.losantos@gmail.com>
2017-09-20 18:29:48 +0200

    feat: use JSONViewer in responses table (#187)
    * feat: use JSONViewer in responses table
    
    * Hopefully fix scrollbar issue AND sticky header
    
    * style table head
    
    * standard js
    
    * fix: replace vW/vH with vw/vh
    
    * Feat: collapsible items in table view
    
    * fix sass lint

--- gather2-ui/ui/assets/apps/response/ResponsesList.jsx
+++ gather2-ui/ui/assets/apps/response/ResponsesList.jsx
@@ -1,55 +1,147 @@
 import React, { Component } from 'react'
-import { FormattedDate, FormattedMessage } from 'react-intl'
+import {
+  FormattedDate,
+  FormattedMessage,
+  FormattedRelative,
+  FormattedTime
+} from 'react-intl'
+
+import { flatten, inflate } from '../utils/types'
+import { JSONViewer } from '../components'
+
+const SEPARATOR = '¬¬¬'  // very uncommon string
 
 export default class ResponsesList extends Component {
   render () {
     const {list} = this.props
 
+    if (list.length === 0) {
+      return <div data-qa='responses-list-empty' />
+    }
+
+    // the first entry will decide the table columns
+    const columns = Object.keys(flatten(list[0].data, SEPARATOR))
+
     return (
       <div data-qa='responses-list' className='x-0'>
-        {
-          list.length > 0 &&
-          <div className='survey-content'>
-            <table className='table table-bordered table-hover table-sm'>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>
-                    <FormattedMessage
-                      id='response.list.table.created'
-                      defaultMessage='Submitted' />
-                  </th>
-                  <th>
-                    <FormattedMessage
-                      id='response.list.table.data'
-                      defaultMessage='Data' />
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                { list.map((response, index) => this.renderResponse(response, index)) }
-              </tbody>
-            </table>
-          </div>
-        }
+        <div className='survey-content'>
+          <table className='table table-sm'>
+            { this.renderHeader(columns) }
+            <tbody>
+              { list.map((response, index) => this.renderResponse(response, index, columns)) }
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
 
-  renderResponse (response, index) {
+  renderHeader (columns) {
+    /****************************************************************
+        Data
+        ====
+        {
+          a: {
+            b: {
+              c: 1,
+              d: 2
+            },
+            e: {
+              f: true
+            },
+            g: []
+          },
+          h: 0
+        }
+
+        Table header
+        ============
+        +---+------------+------+----------------+---+
+        | # | Submitted  | Data | A              | H |
+        |   |            |      +-------+---+----+   |
+        |   |            |      | B     | E | G  |   |
+        |   |            |      +---+---+---+    |   |
+        |   |            |      | C | D | F |    |   |
+        +---+------------+------+---+---+---+----+---+
+        | 1 | 1999-01-01 | #### | 1 | 2 | T | [] | 0 |
+        +---+------------+------+---+---+---+----+---+
+
+    ****************************************************************/
+
+    const headers = inflate(columns, SEPARATOR)
+    const rows = headers.length
+
+    return (
+      <thead>
+        {
+          headers.map((row, index) => (
+            <tr key={index}>
+              {
+                (index === 0) &&
+                <th rowSpan={rows} />
+              }
+              {
+                (index === 0) &&
+                <th rowSpan={rows}>
+                  <FormattedMessage
+                    id='response.list.table.created'
+                    defaultMessage='Submitted' />
+                </th>
+              }
+
+              {
+                Object.keys(row).map(column => (
+                  <th
+                    key={row[column].key}
+                    title={row[column].path}
+                    rowSpan={row[column].isLeaf ? (rows - index) : 1}
+                    colSpan={row[column].siblings}>
+                    { row[column].label }
+                  </th>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </thead>
+    )
+  }
+
+  renderResponse (response, index, columns) {
+    const flattenData = flatten({...response.data}, SEPARATOR)
+
     return (
       <tr data-qa={`response-row-${response.id}`} key={response.id}>
         <td scope='row'>{this.props.start + index}</td>
         <td>
-          <FormattedDate
-            value={response.created}
-            year='numeric'
-            month='short'
-            day='numeric' />
+          <span className='mr-2'>
+            <FormattedDate
+              value={response.created}
+              year='numeric'
+              month='long'
+              day='numeric' />
+          </span>
+          <span className='mr-2'>
+            <FormattedTime
+              value={response.created}
+              hour12={false}
+              hour='2-digit'
+              minute='2-digit'
+              second='2-digit'
+              timeZoneName='short' />
+          </span>
+          <span>
+            (<FormattedRelative value={response.created} />)
+          </span>
         </td>
-        <td>
-          <pre>{JSON.stringify(response.data, 0, 2)}</pre>
-        </td>
+
+        {
+          columns.map(key => (
+            <td key={key}>
+              <JSONViewer data={flattenData[key]} />
+            </td>
+          ))
+        }
       </tr>
     )
   }
