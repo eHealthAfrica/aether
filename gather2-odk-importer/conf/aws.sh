551e86d64088a777cb2b351e0fe228f8cf26b1aa
William Pink <will.pink@gmail.com>
2017-09-28 15:29:54 +0200

    Fix path to template (#213)
    * wrong path
    
    * chore: update file paths

--- gather2-odk-importer/conf/aws.sh
+++ gather2-odk-importer/conf/aws.sh
@@ -3,7 +3,7 @@
 # S3 secrets
 configure_aws_cli () {
   mkdir -p ~/.aws
-  envsubst < /code/conf/aws_config.tmpl > ~/.aws/config
+  envsubst < /code/conf/extras/aws_config.tmpl > ~/.aws/config
   export AWS_PROFILE=assume_role
   eval $(aws s3 cp --sse AES256 s3://ecs-secrets-prod/$APP-$PROJECT - )
 }
