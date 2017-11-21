d1a3f06d83a073811378710a4961da4084e9e56b
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2017-11-21 17:55:42 +0100

    load different fixtures for odk and couchdb

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -7,7 +7,10 @@ function prepare_and_test_container() {
   echo "_____________________________________________ Starting $1 tasks"
   $DC_TEST build $container
   $DC_TEST run $container setuplocaldb
-  docker-compose -f docker-compose-test.yml run core-test manage loaddata dump_empty_schema.json
+  if [[ $2 ]]
+  then
+    docker-compose -f docker-compose-test.yml run core-test manage loaddata $2
+  fi
   $DC_TEST run $container test --noinput
   echo "_____________________________________________ $1 tasks done"
 }
@@ -34,13 +37,14 @@ echo "_____________________________________________ Starting core"
 $DC_TEST up -d core-test
 
 # test and start a clean ODK TEST container
-prepare_and_test_container odk-importer
+prepare_and_test_container odk-importer dump_empty_schema.json
 
 echo "_____________________________________________ Starting odk-importer"
 $DC_TEST up -d odk-importer-test
 
+# docker-compose -f docker-compose-test.yml run core-test manage loaddata dump_empty_schema.json
 # test a clean SYNC TEST container
-prepare_and_test_container couchdb-sync
+prepare_and_test_container couchdb-sync dump.json
 
 # FIXME: run ui tests
 # # test a clean UI TEST container
