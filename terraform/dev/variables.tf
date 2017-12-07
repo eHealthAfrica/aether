d957d43c3a9f721a1fa8f5113a39ad86d54cae2c
Oluwafemi Olofintuyi <oluwafemi.olofintuyi@gmail.com>
2017-12-07 16:13:07 +0100

    update billing tags

--- terraform/dev/variables.tf
+++ terraform/dev/variables.tf
@@ -1,4 +1,4 @@
 variable "environment" { default = "dev" }
 variable "project" { default = "aether" }
-variable "project_billing_id" { default = "aether" }
+variable "project_billing_id" { default = "47200/NIGXXX" }
 variable "aws_region" { default = "eu-west-1" }
