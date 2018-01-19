b8cd519d2970071729240fb3aab34d6fbbbb34a2
Obdulia Losantos <obdulia.losantos@gmail.com>
2018-01-19 11:04:25 +0100

    test(couchdb-sync): 100% coverage (#55)
    * test(couchdb-sync): 100% coverage
    
    * chore: link common module only in development not tests
    
    * chore: cleanup coverage settings
    
    * tests: check that kernel server is up for testing

--- aether-odk-module/aether/odk/tests/test_views.py
+++ aether-odk-module/aether/odk/tests/test_views.py
@@ -10,3 +10,11 @@ class TestViews(TestCase):
         response = self.client.get(reverse('health'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(), {})
+
+    def test__kernel_check(self):
+        response = self.client.get(reverse('check-kernel'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.content.decode(),
+            'Brought to you by eHealth Africa - good tech for hard places'
+        )
