20d6ef2cd5ec259a05fa6ed22bd4e6e1c6478bdc
MrPink <will.pink@gmail.com>
2018-02-21 12:51:47 +0100

    chore: pass as a build env

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,8 @@
 set -e
 
 IMAGE_REPO='ehealthafrica'
-
-export APPS=( kernel odk )
+APPS=( kernel odk )
+VERSION=`cat VERSION`
 
 for APP in "${APPS[@]}"
 do
@@ -11,7 +11,8 @@ do
 	VERSION=`cat $AETHER_APP/VERSION`
 	echo "version: $VERSION"
   echo "Building Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
-  docker-compose build --build-arg GIT_REVISION=$TRAVIS_COMMIT $APP
+  docker-compose build --build-arg GIT_REVISION=$TRAVIS_COMMIT \
+  --build-arg VERSION=$VERSION $APP
   docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
   docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:latest"
   echo "Pushing Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
