3c708e8a35846dbb4d063ace0695b8ab606f5244
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-03-13 15:52:07 +0100

    wip: everything's running against kernel

--- aether-mock-data/mocker.py
+++ aether-mock-data/mocker.py
@@ -2,6 +2,37 @@ from collections import namedtuple
 import json
 from random import randint, uniform, choice, sample
 import string
+from uuid import uuid4
+
+from aether.client import KernelClient
+
+class Generic(object):
+
+    @staticmethod
+    def boolean():
+        return choice([True, False])
+
+    @staticmethod
+    def float():
+        return uniform(.01, 1000.00)
+
+    @staticmethod
+    def int():
+        return randint(1,99999)
+
+    @staticmethod
+    def null():
+        return None
+
+    @staticmethod
+    def string():
+        size = choice(range(3,12))
+        return "".join(sample(string.ascii_lowercase, size))
+
+    @staticmethod
+    def uuid():
+        return str(uuid4())
+
 
 class DataMocker(object):
     '''
@@ -12,12 +43,15 @@ class DataMocker(object):
         override_property(property_name, fn)
     '''
 
-    def __init__(self, name, schema):
+    def __init__(self, name, schema, parent):
+
+        self.MAX_ARRAY_SIZE = 6
 
         self.name = name
-        self.schema = schema
+        self.raw_schema = schema
+        self.parent = parent
         self.subschema = {}
-        primative_types = [
+        self.primative_types = [
             "null",
             "boolean",
             "int",
@@ -30,58 +64,76 @@ class DataMocker(object):
 
         self.type_methods = {
             primative : MockFn(self._default(primative))
-            for primative in primative_types
+            for primative in self.primative_types
         }
+        self.created = []
         self.property_methods = {}
+        self.required = []
         self.ignored_properties = []
         self.restricted_types = {}
         self.instructions = {}
+        self.override_property("id", MockFn(Generic.uuid))
         self.load()
 
     def _default(self, primative):
         if primative in ["int", "long"]:
-            return self._gen_int
+            return Generic.int
         if primative in ["float", "double"]:
-            return self._gen_float
+            return Generic.float
         if primative is "null":
-            return self._gen_null
+            return Generic.null
         if primative is "string":
-            return self._gen_string
+            return Generic.string
         if primative is "boolean":
-            return self._gen_boolean
+            return Generic.boolean
+
+    def get_reference(self, exclude=None):
+        # returns an ID, either of by registering a new instance
+        # or by returning a value from created
+        if len(self.created) == 0:
+            new_record = self.get()
+            self.parent.register(self.name, new_record)
+            _id = new_record.get("id")
+        else:
+            _id = self.created[0]
+        return _id
+        '''
+        count = len(self.created)
+        thresh =  0 # if count <= 80 else 80
+        new = (randint(0,100) >= thresh)
+        if new:
+            new_record = self.get()
+            self.parent.register(self.name, new_record)
+            _id = new_record.get("id")
+        else:
+            _id = choice(self.created[:-40])
+        return _id
+        '''
 
     def get(self, record_type="default"):
         # Creates a mock instance.
         if record_type is "default":
-            return _get(self.name)
+            return self._get(self.name)
         else:
-            return _get(record_type)
+            return self._get(record_type)
 
     def _get(self, name):
         instructions = self.instructions.get(name)
         if not instructions:
             raise ValueError("No instuctions for type %s" % name)
         body = {}
-        for name, fn in instuctions:
+        for name, fn in instructions:
+            print("\n%s*** ||| %s" % (name, fn))
             body[name] = fn()
+        self.created.append(body.get('id'))
         return body
 
-
-    def _gen_boolean(self):
-        return choice([True, False])
-
-    def _gen_float(self):
-        return uniform(.01, 1000.00)
-
-    def _gen_int(self):
-        return randint(1,99999)
-
-    def _gen_null(self):
-        return None
-
-    def _gen_string(self):
-        size = choice(xrange(3,12))
-        return "".join(sample(string.ascii_lowercase, size))
+    def _dispatch_complex(self, name):
+        try:
+            return self._get(name)
+        except ValueError as verr:
+            fn = self.gen("null")
+            return fn()
 
     def gen(self, _type):
         return self.type_methods.get(_type)
@@ -90,26 +142,51 @@ class DataMocker(object):
         fn = self.gen(_type)
         return MockFn(self._gen_array, [fn])
 
-    def gen_random_type(self, _types=[]):
-        return MockFn(self._gen_random_type, [types])
+    def gen_random_type(self, name=None, _types=[]):
+        return MockFn(self._gen_random_type, [name, _types])
 
-    def _gen_random_type(self, types):
+    def _gen_random_type(self, name, types):
+        if name in self.required:
+            types = [i for i in types if i != "null"]
         _type = choice(types)
-        if not isinstance(_type, string)
-            if not _type.get("type", None) is "array":
+        print("%s ---> %s" % (types, _type))
+        fn = None
+        if isinstance(_type, dict):
+            if _type.get("type", None) != "array":
                 raise ValueError("unexpected type, %s" % _type.get('type'))
             items = _type.get("items")
             fn = self.gen_array(items)
             return fn()
-        fn = self.gen(_type)
+        elif isinstance(_type, list):
+            if name in self.required:
+                _type = [i for i in _types if i != "null"]
+            _type = choice(_type)
+        if not _type in self.primative_types:
+            fn = self.gen_complex(_type)
+        else:
+            fn = self.gen(_type)
+        print(fn)
         return fn()
 
     def _gen_array(self, fn):
-        size = choice(xrange(2,12))
-        return [fn() for i in xrange(size)]
+        size = choice(range(2,self.MAX_ARRAY_SIZE))
+        return [fn() for i in range(size)]
 
     def gen_complex(self, _type):
-        return self.gen("int")  # TODO implement complex lookups
+        return MockFn(self._dispatch_complex, _type)
+
+    def gen_reference(self, name, _type, types):
+        return MockFn(self._gen_reference, [name, _type, types])
+
+    def _gen_reference(self, name, _type, types):
+        if name in self.required:
+            types = [i for i in types if i != "null"]
+        chosen = choice(types)
+        if isinstance(chosen, str):
+            return self.parent.get_reference(_type)
+        else:
+            size = choice(range(2, self.MAX_ARRAY_SIZE))
+            return [self.get_reference(_type) for i in range(size)]
 
     def ignore(self, property_name):
         # turn off mocking for this property
@@ -124,8 +201,9 @@ class DataMocker(object):
         self.load()
 
     def load(self):
-        self.schema = json.loads(schema)
-
+        self.schema = json.loads(self.raw_schema)
+        for obj in self.schema:
+            self.parse(obj)
 
     def parse(self, schema):
         # looks at all the types called for
@@ -136,27 +214,34 @@ class DataMocker(object):
         fields = schema.get("fields", [])
         for field in fields:
             instructions.append(self._comprehend_field(field))
+        self.instructions[name] = instructions
 
     def _comprehend_field(self, field):
         name = field.get("name")
         if name in self.ignored_properties:
-            return tuple(name, self.gen("null"))  # Return null function and get out
-        sub_type = None
+            return (name, self.gen("null"))  # Return null function and get out
         try:
-            sub_type = field.get("jsonldPredicate", {}).get("_id")
-            return tuple(name, self.gen_complex(name))  # This is a reference property
+            ref_type = field.get("jsonldPredicate").get("_id")
+            types = field.get('type')
+            return (name, self.gen_reference(name, ref_type, types))  # This is a reference property  # TODO THIS MIGHT WANT TO BE sub_type
         except Exception as err:
-            pass
+            print(err)
+            pass  # This is simpler than checking to see if this is a dictionary?
         if name in self.property_methods.keys():
-            return tuple(name, property_methods.get(name))  # We have an explicit method for this
+            return (name, self.property_methods.get(name))  # We have an explicit method for this
         types = field.get("type")
-        if isinstance(types, string):
-            return tuple(name, self.gen(types))  # Single type for this field
+        if isinstance(types, str):
+            return (name, self.gen(types))  # Single type for this field
         if name in self.restricted_types.keys():  # we've limited the types we want to mock
             types = list(set(types).union(set(self.restricted_types.get(name))))
-        return tuple(name, self.gen_random_type(types))
+        return tuple([name, self.gen_random_type(name, types)])
 
-
+    def require(self, *property):
+        # Make a field never resolve to null (if null is an option)
+        if isinstance(property, list):
+            self.required.extend(property)
+        else:
+            self.required.append(property)
 
 
     def restrict_type(self, property_name, allowable_types=[]):
@@ -185,7 +270,34 @@ class MockingManager(object):
     def __init__(self):
         # connects to Aether and gets available schemas.
         # constructs a DataMocker for each type
-        pass
+        kernel_url = "http://kernel.aether.local:8000/v1"
+        kernel_credentials ={
+            "username": "admin-kernel",
+            "password": "adminadmin",
+        }
+        self.client = KernelClient(kernel_url, **kernel_credentials)
+        self.types = {}
+        self.load()
 
-    def proxy(self, proxy_type, reference_field):
-        pass
+    def get(self, _type):
+        if not _type in self.types.keys():
+            raise KeyError("No schema for type %s" % (_type))
+        return self.types.get(_type).get()
+
+    def get_reference(self, _type):
+        if not _type in self.types.keys():
+            raise KeyError("No schema for type %s" % (_type))
+        return self.types.get(_type).get_reference()
+
+    def register(self, name, payload):
+        pprint(payload)
+
+    def load(self):
+        for schema in self.client.Resource.Schema:
+            name = schema.get("name")
+            definition = schema.get('definition')
+            full_name = [obj.get("name") for obj in definition if obj.get('name').endswith(name)][0]
+            self.types[full_name] = DataMocker(full_name, json.dumps(definition), self)
+
+def pprint(obj):
+    print(json.dumps(obj, indent=2))
