2ff2cbaefabd9a9c8b0c55bcb965327dcf33ea9f
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-03-20 14:51:50 +0100

    chore: moved data mocker into aether.mocker namespace. Created setup.py

--- aether-utils/aether-mock-data/setup.py
+++ aether-utils/aether-mock-data/setup.py
@@ -1,14 +1,18 @@
 import os
 from setuptools import setup, find_packages
+import sys
+
+if sys.version_info < (3,0):
+    sys.exit('aether data mocker requires Python 3.x')
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='aether.client',
+    name='aether.mocker',
     version='0.0.0',
     install_requires=[
-        "requests"
+        "aether-client"
     ],
     packages=find_packages(),
     namespace_packages=['aether']
