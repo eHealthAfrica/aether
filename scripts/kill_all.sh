9926ac4daa01e8c766d83b4c18b57763604e1e6a
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-03-20 10:49:45 +0100

    chore:update kill_all.sh to contain client build container

--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -5,4 +5,5 @@ docker-compose kill
 docker-compose -f docker-compose-base.yml      kill
 docker-compose -f docker-compose-connect.yml   kill
 docker-compose -f docker-compose-common.yml    kill
+docker-compose -f docker-compose-client.yml    kill
 docker-compose -f docker-compose-test.yml      kill
