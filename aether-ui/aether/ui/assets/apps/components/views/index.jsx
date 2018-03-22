e37f92f281b0772df6235d8c695e7657d08d8d56
Sabine Claassen <been@b33n.net>
2018-03-22 14:12:57 +0100

    feat(index): added navbar

--- aether-ui/aether/ui/assets/apps/components/views/index.jsx
+++ aether-ui/aether/ui/assets/apps/components/views/index.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PipeLines from './pipelines/index'
 
 class Home extends Component {
   render () {
     return (
       <div>
-        This is AUX's Landing Page<br />
+        <div className='navbar'>
+          <a className='navbar-brand' href='/' title='aether'>
+            <div className='logo-container'>
+              <div className='flipper'>
+                <div className='front'>
+                </div>
+                <div className='back'>
+                </div>
+              </div>
+            </div>
+            <span data-app-name='app-name'>Aether</span>
+          </a>
+          <div className='nav-link user'>
+            <span
+              id='logged-in-user-info'>
+              User name
+            </span>
+            <span className='logout'>
+              <a href='#'><i className='fa fa-sign-out' title='Sign Out' aria-hidden='true' ></i></a>
+            </span>
+          </div>
+        </div>
         <div>
-          <Link to='/pipelines'>Pipelines</Link><br />
+          <h1>Project Name</h1>
           <Link to='/new-pipeline'>New Pipeline</Link>
+          <PipeLines></PipeLines>
         </div>
       </div>
     )
