bd9c342ad6c072450f6811583ddffd1b62301d13
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-03-26 12:24:10 +0200

    adding required dockerization to tests

--- scripts/build_client_and_distribute.sh
+++ scripts/build_client_and_distribute.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-DC_CLIENT="docker-compose -f docker-compose-client.yml"
+DC_CLIENT="docker-compose -f docker-compose-build-aether-utils.yml"
 
 # remove previous containers (clean start)
 ./scripts/kill_all.sh
 $DC_CLIENT down
 
-# create the distribution
+# create the client distribution
 $DC_CLIENT build client
 $DC_CLIENT run client build
 
 PCK_FILE=aether.client-0.0.0-py2.py3-none-any.whl
 
 # distribute within the containers
-containers=( producer )
+containers=( producer, test-aether-integration )
 for container in "${containers[@]}"
 do
   if [[ $container = "producer" ]]
@@ -25,5 +25,44 @@ do
   fi
   cp -r ./aether-utils/aether-client/dist/$PCK_FILE ./$FOLDER/conf/pip/dependencies/
 done
+
+# create the mocker distribution
+$DC_CLIENT build mocker
+$DC_CLIENT run mocker build
+
+PCK_FILE=aether.mocker-0.0.0-py2.py3-none-any.whl
+
+# distribute within the containers
+containers=( test-aether-integration )
+for container in "${containers[@]}"
+do
+  if [[ $container = "producer" ]]
+  then
+    FOLDER=aether-$container
+  else
+    FOLDER=aether-$container-module
+  fi
+  cp -r ./aether-utils/aether-mock-data/dist/$PCK_FILE ./$FOLDER/conf/pip/dependencies/
+done
+
+# create the saladbar distribution
+$DC_CLIENT build saladbar
+$DC_CLIENT run saladbar build
+
+PCK_FILE=saladbar-0.0.0-py2.py3-none-any.whl
+
+# distribute within the containers
+containers=( test-aether-integration )
+for container in "${containers[@]}"
+do
+  if [[ $container = "producer" ]]
+  then
+    FOLDER=aether-$container
+  else
+    FOLDER=aether-$container-module
+  fi
+  cp -r ./aether-utils/aether-saladbar/dist/$PCK_FILE ./$FOLDER/conf/pip/dependencies/
+done
+
 
 ./scripts/kill_all.sh
--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-docker-compose kill
-docker-compose -f docker-compose-base.yml      kill
-docker-compose -f docker-compose-connect.yml   kill
-docker-compose -f docker-compose-common.yml    kill
-docker-compose -f docker-compose-client.yml    kill
-docker-compose -f docker-compose-test.yml      kill
+docker-compose 							kill
+docker-compose -f docker-compose-base.yml      			kill
+docker-compose -f docker-compose-connect.yml   			kill
+docker-compose -f docker-compose-common.yml    			kill
+docker-compose -f docker-compose-build-aether-utils.yml    	kill
+docker-compose -f docker-compose-test.yml      			kill
