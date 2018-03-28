072251601a7cbbb8a53fc6799db8cb45a1ee135a
Sabine Claassen <been@b33n.net>
2018-03-28 18:07:52 +0200

    feat(pipeline): added markup for pipeline nav

--- aether-ui/aether/ui/assets/apps/components/views/pipelines/pipeline.jsx
+++ aether-ui/aether/ui/assets/apps/components/views/pipelines/pipeline.jsx
@@ -2,11 +2,49 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class PipeLine extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      pipelineView: 'input'
+    }
+  }
 
   render () {
     return (
-      <div className='pipeline'>
-      </div>
+      <div className={`pipeline pipeline--${this.state.pipelineView}`}>
+        <div className='pipeline-nav'>
+          <div
+            className='pipeline-nav-item__input'
+            onClick={() => this.setState({ pipelineView: 'input' })}>
+            <div className='badge'>1</div>
+            Input
+          </div>
+          <div
+            className='pipeline-nav-item__entityTypes'
+            onClick={() => this.setState({ pipelineView: 'entityTypes' })}>
+            <div className='badge'>2</div>
+            Entity Types
+          </div>
+          <div
+            className='pipeline-nav-item__mapping'
+            onClick={() => this.setState({ pipelineView: 'mapping' })}>
+            <div className='badge'>3</div>
+            mapping
+          </div>
+          <div className='pipeline-nav-item__output'>
+            <div className='badge badge-small' />
+            output
+            <div className='output-toggle' />
+          </div>
+        </div>
+
+        <div className='pipeline-sections'>
+          <div className='pipeline-section__input'>input</div>
+          <div className='pipeline-section__entityTypes'>entity types</div>
+          <div className='pipeline-section__mapping'>mapping</div>
+        </div>
+        <div className='pipeline-output'>output</div>
+      </div> 
     )
   }
 }
