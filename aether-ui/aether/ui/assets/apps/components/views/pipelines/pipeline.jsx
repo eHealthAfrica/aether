c37b5a831a2e7fd12a94f6f329906c6cd3acfa61
Sabine Claassen <been@b33n.net>
2018-03-29 12:06:23 +0200

    feat(pipeline): transitions for all section containers

--- aether-ui/aether/ui/assets/apps/components/views/pipelines/pipeline.jsx
+++ aether-ui/aether/ui/assets/apps/components/views/pipelines/pipeline.jsx
@@ -5,33 +5,38 @@ class PipeLine extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      pipelineView: 'input'
+      pipelineView: 'input',
+      showOutput: false
     }
   }
 
   render () {
     return (
-      <div className={`pipeline pipeline--${this.state.pipelineView}`}>
+      <div className={`pipeline pipeline--${this.state.pipelineView} ${this.state.showOutput ? 'show-output' : ''}`}>
         <div className='pipeline-nav'>
-          <div
-            className='pipeline-nav-item__input'
-            onClick={() => this.setState({ pipelineView: 'input' })}>
-            <div className='badge'>1</div>
-            Input
+          <div className='pipeline-nav-items'>
+            <div
+              className='pipeline-nav-item__input'
+              onClick={() => this.setState({ pipelineView: 'input' })}>
+              <div className='badge'>1</div>
+              Input
+            </div>
+            <div
+              className='pipeline-nav-item__entityTypes'
+              onClick={() => this.setState({ pipelineView: 'entityTypes' })}>
+              <div className='badge'>2</div>
+              Entity Types
+            </div>
+            <div
+              className='pipeline-nav-item__mapping'
+              onClick={() => this.setState({ pipelineView: 'mapping' })}>
+              <div className='badge'>3</div>
+              mapping
+            </div>
           </div>
           <div
-            className='pipeline-nav-item__entityTypes'
-            onClick={() => this.setState({ pipelineView: 'entityTypes' })}>
-            <div className='badge'>2</div>
-            Entity Types
-          </div>
-          <div
-            className='pipeline-nav-item__mapping'
-            onClick={() => this.setState({ pipelineView: 'mapping' })}>
-            <div className='badge'>3</div>
-            mapping
-          </div>
-          <div className='pipeline-nav-item__output'>
+            className='pipeline-nav-item__output'
+            onClick={() => this.toggleOutput()}>
             <div className='badge badge-small' />
             output
             <div className='output-toggle' />
@@ -44,8 +49,17 @@ class PipeLine extends Component {
           <div className='pipeline-section__mapping'>mapping</div>
         </div>
         <div className='pipeline-output'>output</div>
-      </div> 
+      </div>
     )
+  }
+
+  toggleOutput () {
+    console.log(this.state.showOutput)
+     if (!this.state.showOutput) {
+      this.setState({ showOutput: true })
+    } else {
+      this.setState({ showOutput: false })
+    }
   }
 }
 
