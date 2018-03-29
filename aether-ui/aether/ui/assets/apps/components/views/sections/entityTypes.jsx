a284dd25e67b0f8b5da9e5de181a8b69c758d984
Sabine Claassen <been@b33n.net>
2018-03-29 15:34:49 +0200

    fix(js): removed empty lines

--- aether-ui/aether/ui/assets/apps/components/views/sections/entityTypes.jsx
+++ aether-ui/aether/ui/assets/apps/components/views/sections/entityTypes.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class EntityTypes extends Component {
-
   render () {
     return (
       <div className='section-body'>
