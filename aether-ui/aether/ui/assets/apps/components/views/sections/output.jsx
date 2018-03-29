d34a7415753d8cee0b449656ae7edf27f348df75
Sabine Claassen <been@b33n.net>
2018-03-29 15:33:57 +0200

    feat(pipeline): added stylesheets for all sections

--- aether-ui/aether/ui/assets/apps/components/views/sections/output.jsx
+++ aether-ui/aether/ui/assets/apps/components/views/sections/output.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 class Output extends Component {
-
   render () {
     return (
       <div className='section-body'>
