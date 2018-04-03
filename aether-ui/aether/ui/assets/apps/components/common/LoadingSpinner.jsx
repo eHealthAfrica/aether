c00d745d57268414410db8fbc2759651dbf101a0
Sabine Claassen <been@b33n.net>
2018-04-03 16:40:09 +0200

    improved fullscreen toggle style

--- aether-ui/aether/ui/assets/apps/components/common/LoadingSpinner.jsx
+++ aether-ui/aether/ui/assets/apps/components/common/LoadingSpinner.jsx
@@ -13,12 +13,12 @@ export default class LoadingSpinner extends Component {
       <div data-qa='data-loading' className='loading-container'>
         <div className='loading-info'>
           <div className='loading-spinner'>
-          <div className='dot1' />
-          <div className='dot2' />
-          <div className='dot3' />
-          <div className='dot4' />
-          <div className='dot5' />
-          <div className='dot6' />
+            <div className='dot1' />
+            <div className='dot2' />
+            <div className='dot3' />
+            <div className='dot4' />
+            <div className='dot5' />
+            <div className='dot6' />
           </div>
           <FormattedMessage
             id='alert.loading'
