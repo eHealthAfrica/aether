6c9bcee8501c8793e7c1cca269fccce5a636977e
Obdulia Losantos <obdulia.losantos@gmail.com>
2018-04-09 13:58:13 +0200

    chore(ui): tests setup (#127)
    * chore: upgrade dependencies and more (#126)
    
    * chore: upgrade pip 9.0.3
    
    * chore: update setup.py files
    
    * chore: upgrade python dependencies
    
    * chore(docker): install sudo package
    
    * chore: new rest-framework decorator
    
    * chore: check kernel url only if needed
    
    * chore: more upgrades
    
    * chore(ui): distribute common module
    
    * chore(ui): fix sass-lint tests
    
    * chore(ui): standard --format

--- aether-ui/aether/ui/assets/apps/redux/reducers.js
+++ aether-ui/aether/ui/assets/apps/redux/reducers.js
@@ -1,6 +1,6 @@
-import { combineReducers } from 'redux';
-import samples from './modules/sample_reducer';
+import { combineReducers } from 'redux'
+import samples from './modules/sample_reducer'
 
 export default combineReducers({
   samples
-});
+})
