ee7c7d6cfbd79152898f9048b7e0310ab97b3fe7
obdulia <obdulia.losantos@gmail.com>
2018-04-10 16:52:18 +0200

    test(ui): implement utils tests

---
+++ aether-ui/aether/ui/assets/apps/utils/index.spec.jsx
@@ -0,0 +1,18 @@
+/* global describe, it, expect */
+
+import { generateGUID } from './index'
+
+describe('utils', () => {
+  describe('generateGUID', () => {
+    it('should match the simplest GUID regex', () => {
+      const uuid = generateGUID()
+
+      expect(uuid).toBeTruthy()
+      expect(uuid.length).toEqual(36)
+      expect(uuid.charAt(8)).toEqual('-')
+      expect(uuid.charAt(13)).toEqual('-')
+      expect(uuid.charAt(18)).toEqual('-')
+      expect(uuid.charAt(23)).toEqual('-')
+    })
+  })
+})
