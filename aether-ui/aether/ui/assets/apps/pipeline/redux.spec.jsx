0a3d83138620c9118b2c7419b5e0dbd42a15b039
lordmallam <lordy002000@gmail.com>
2018-04-18 11:47:17 +0100

    fix: lint

--- aether-ui/aether/ui/assets/apps/pipeline/redux.spec.jsx
+++ aether-ui/aether/ui/assets/apps/pipeline/redux.spec.jsx
@@ -7,7 +7,7 @@ describe('Pipeline redux', () => {
 })
 
 describe('Pipeline actions', () => {
-  it('should create an action when selected ', () => {
+  it('should create an action when selected pipeline is changed', () => {
     const selectedPipeline = { name: 'mock name', id: 1, errors: 0, entityTypes: 3 }
     const expectedAction = {
       type: types.SELECTED_PIPELINE_CHANGED,
