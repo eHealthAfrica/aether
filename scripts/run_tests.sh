80b99a303bcf4e92e5c112c1072052c83e0ae722
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-04-23 21:35:35 +0200

    clean up kernel yaml files

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,8 +2,10 @@ set -x
 
 NAMESPACE=test
 
-docker-compose build kernel
+# docker-compose build
+kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
 
+# TODO: better name
 getRecent () {
     kubectl get pods --sort-by=.status.startTime -l app=$1 --no-headers | tail -n 1 | awk '{print $1}'
 }
@@ -11,3 +13,5 @@ getRecent () {
 kubectl exec --namespace=$NAMESPACE -it $(getRecent kernel) --container kernel -- bash /code/entrypoint.sh setuplocaldb
 
 kubectl exec --namespace=$NAMESPACE -it $(getRecent kernel) --container kernel -- bash /code/entrypoint.sh test_coverage
+
+# kubectl exec -it $(kubectl get pods | grep kernel | awk '{print $1}') --container kernel -- bash /code/entrypoint.sh manage loaddata aether/kernel/api/tests/fixtures/project_empty_schema.json
