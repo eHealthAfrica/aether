b48ed1108ae4a4df8821e0ad2412972fd1dbc56f
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-04-23 12:40:18 +0200

    fix code mount

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -6,16 +6,16 @@ NAMESPACE=test
 kubectl create namespace $NAMESPACE
 kubectl config set-context $(kubectl config current-context) --namespace=$NAMESPACE
 
-# helm del --purge db
-# helm del --purge kernel
-# helm del --purge odk
+helm del --purge db
+helm del --purge kernel
+helm del --purge odk
 
-# kubectl delete --all pods --namespace=$NAMESPACE
-# kubectl delete --all deployments --namespace=$NAMESPACE
-# kubectl delete --all services --namespace=$NAMESPACE
-# kubectl delete --all secrets --namespace=$NAMESPACE
-# kubectl delete --all persistentvolumeclaims --namespace=$NAMESPACE
-# kubectl delete --all persistentvolumes --namespace=$NAMESPACE
+kubectl delete --all pods --namespace=$NAMESPACE
+kubectl delete --all deployments --namespace=$NAMESPACE
+kubectl delete --all services --namespace=$NAMESPACE
+kubectl delete --all secrets --namespace=$NAMESPACE
+kubectl delete --all persistentvolumeclaims --namespace=$NAMESPACE
+kubectl delete --all persistentvolumes --namespace=$NAMESPACE
 
 kubectl create -f ./helm/dev-secrets/secrets.yaml
 kubectl create -f ./helm/dev-secrets/database-secrets.yaml
@@ -26,6 +26,6 @@ kubectl rollout status deployment db
 helm install --name kernel helm/kernel
 kubectl rollout status deployment kernel
 
-kubectl exec --namespace=$NAMESPACE -it $(kubectl get pods | grep kernel | awk '{print $1}') --container kernel -- bash /code/entrypoint.sh setuplocaldb
+# kubectl exec --namespace=$NAMESPACE -it $(kubectl get pods | grep kernel | awk '{print $1}') --container kernel -- bash /code/entrypoint.sh setuplocaldb
 
-kubectl exec --namespace=$NAMESPACE -it $(kubectl get pods | grep kernel | awk '{print $1}') --container kernel -- bash /code/entrypoint.sh test_coverage
+# kubectl exec --namespace=$NAMESPACE -it $(kubectl get pods | grep kernel | awk '{print $1}') --container kernel -- bash /code/entrypoint.sh test_coverage
