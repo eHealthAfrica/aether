06c5d4829adde875b5294d64b529f8bb74d6703c
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-04-25 15:41:09 +0200

    move kubernetes scripts to a separate directory

---
+++ scripts/kubernetes/run_travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./scripts/kubernetes/install_minikube.sh
+./scripts/kubernetes/install_helm.sh
+./scripts/kubernetes/start_minikube.sh
+./scripts/kubernetes/start_helm.sh
+./scripts/kubernetes/start_test_cluster.sh
+./scripts/kubernetes/run_tests.sh test_all
--- scripts/run_travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-./scripts/install_minikube.sh
-./scripts/install_helm.sh
-./scripts/start_minikube.sh
-./scripts/start_helm.sh
-./scripts/start_test_cluster.sh
-./scripts/run_tests.sh test_all
