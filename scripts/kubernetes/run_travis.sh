58f366f0cfc45082b3364f2ba5c2573508d597f5
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-04-29 11:38:16 +0200

    pin helm version

--- scripts/kubernetes/run_travis.sh
+++ scripts/kubernetes/run_travis.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 ./scripts/kubernetes/install_minikube.sh
-./scripts/kubernetes/install_helm.sh
+./scripts/kubernetes/install_helm.sh --version v2.8.1
 ./scripts/kubernetes/start_minikube.sh
 ./scripts/kubernetes/start_helm.sh
-kubectl create namespace test && kubectl config set-context $(kubectl config current-context) --namespace=test
 ./scripts/kubernetes/install_secrets.sh
 ./scripts/kubernetes/start_cluster.sh ./helm/overrides/test
 ./scripts/kubernetes/run_tests.sh test_all
--- scripts/kubernetes/start_cluster.sh
+++ scripts/kubernetes/start_cluster.sh
@@ -6,26 +6,17 @@ VALUES_DIR=$1
 
 docker-compose build kernel odk
 
-helm install stable/postgresql --name db --values=./helm/overrides/db.yaml
+helm install stable/postgresql \
+     --name db \
+     --values=./helm/overrides/db.yaml
 kubectl rollout status deployment db
 
-helm install --name kernel helm/kernel --values=$VALUES_DIR/kernel.yaml
+helm install helm/kernel \
+     --name kernel \
+     --values=./$VALUES_DIR/kernel.yaml
 kubectl rollout status deployment kernel
 
-helm install --name odk helm/odk --values=$VALUES_DIR/odk.yaml
+helm install helm/odk \
+     --name odk \
+     --values=$VALUES_DIR/odk.yaml
 kubectl rollout status deployment odk
-
-# helm install stable/postgresql \
-#      --name db \
-#      --values=./helm/overrides/db.yaml
-# kubectl rollout status deployment db
-
-# helm install helm/kernel \
-#      --name kernel \
-#      --values=./$VALUES_DIR/kernel.yaml
-# kubectl rollout status deployment kernel
-
-# helm install helm/odk \
-#      --name odk \
-#      --values=$VALUES_DIR/odk.yaml
-# kubectl rollout status deployment odk
