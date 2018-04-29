872dd9a86888305dfe19f4a4c618badd55e5fdda
Fredrik Wallberg <fredrik.wallberg@gmail.com>
2018-04-29 11:02:24 +0200

    test namespace fix

--- scripts/kubernetes/run_travis.sh
+++ scripts/kubernetes/run_travis.sh
@@ -4,6 +4,7 @@
 ./scripts/kubernetes/install_helm.sh
 ./scripts/kubernetes/start_minikube.sh
 ./scripts/kubernetes/start_helm.sh
+kubectl create namespace test && kubectl config set-context $(kubectl config current-context) --namespace=test
 ./scripts/kubernetes/install_secrets.sh
 ./scripts/kubernetes/start_cluster.sh ./helm/overrides/test
 ./scripts/kubernetes/run_tests.sh test_all
