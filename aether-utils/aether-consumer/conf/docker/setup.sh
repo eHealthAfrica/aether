8aa38265d11986e4d6273252d65d04a063dc810d
shawnsarwar <ssarwar@gmail.com>
2018-05-04 12:26:58 +0200

    feature: aether.consumer library (#139)
    * feat: test scaffolding running in python2 and python3
    
    * feat: tests reading and writing against Kafka, using stub of new SDK consumer
    
    * feat: implemented get_approval_filter in consumer SDK
    
    * feat: masking working with default and custom filters in unit tests
    
    * feature: aether.consumer library complete and ready for pypi push
    
    * chore: cleanup / response to comments on PR

---
+++ aether-utils/aether-consumer/conf/docker/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+
+
+################################################################################
+# define variables
+################################################################################
+
+# Do not buffer stdout so we see log output immediatly
+export PYTHONUNBUFFERED=true
