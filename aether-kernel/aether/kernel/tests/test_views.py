03d6bd2994beba9efc20e0aa4fefe9ed2a418e47
Alvaro Mouriño <alvaro.mourino@ehealthafrica.org>
2018-05-15 13:46:55 +0200

    Kong

--- aether-kernel/aether/kernel/tests/test_views.py
+++ aether-kernel/aether/kernel/tests/test_views.py
@@ -28,3 +28,20 @@ class TestViews(TestCase):
         response = self.client.get(reverse('health'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.json(), {})
+
+    def test_api_pass(self):
+        response = self.client.post('/token', {'client_id': 'a_test_client_id'})
+        self.assertEqual(400, response.status_code)
+        response = self.client.post('/token', {'username': 'testUsername', 'password': 'testPassword'})
+        self.assertEqual(200, response.status_code)
+        response = self.client.post('/token',
+                                    {'username': 'testUsername', 'password': 'testPassword',
+                                     'redirect_uri': 'http://testuri.org', 'app_name': 'test_app'})
+        self.assertEqual(200, response.status_code)
+
+    def test__urls__api_pass(self):
+        self.assertEqual(
+            reverse('token'),
+            '/token',
+            'There is a "/token" endpoint'
+        )
