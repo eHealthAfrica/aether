95b75a17f24453b29847e61a71863b673794adc4
lordmallam <lordy002000@gmail.com>
2018-05-15 12:51:35 +0100

    fix: tests - push to kernel

--- aether-ui/aether/ui/api/tests/test_utils.py
+++ aether-ui/aether/ui/api/tests/test_utils.py
@@ -4,26 +4,25 @@ from django.test import TestCase
 
 from . import PIPELINE_EXAMPLE_1
 from ..models import Pipeline
-from ..utils import (kernel_data_request, create_new_kernel_object, is_object_linked,
-                     create_project_schema_object)
+from .. import utils
 
 
 class ViewsTest(TestCase):
     def test_kernel_data_request(self):
-        result = kernel_data_request('projects', 'get')
+        result = utils.kernel_data_request('projects', 'get')
         self.assertIn('count', result)
         with self.assertRaises(Exception):
-            kernel_data_request('projects', 'post', {'wrong-input': 'tests'})
+            utils.kernel_data_request('projects', 'post', {'wrong-input': 'tests'})
 
     def test_create_new_kernel_object(self):
         pipeline = Pipeline.objects.create(
-          name=PIPELINE_EXAMPLE_1['name'],
-          schema=PIPELINE_EXAMPLE_1['schema'],
-          input=PIPELINE_EXAMPLE_1['input'],
-          entity_types=PIPELINE_EXAMPLE_1['entity_types'],
-          mapping=PIPELINE_EXAMPLE_1['mapping'],
-          output=PIPELINE_EXAMPLE_1['output'],
-          kernel_refs=PIPELINE_EXAMPLE_1['kernel_refs']
+            name=PIPELINE_EXAMPLE_1['name'],
+            schema=PIPELINE_EXAMPLE_1['schema'],
+            input=PIPELINE_EXAMPLE_1['input'],
+            entity_types=PIPELINE_EXAMPLE_1['entity_types'],
+            mapping=PIPELINE_EXAMPLE_1['mapping'],
+            output=PIPELINE_EXAMPLE_1['output'],
+            kernel_refs=PIPELINE_EXAMPLE_1['kernel_refs']
         )
         project_data = {
                         'revision': '123',
@@ -40,17 +39,17 @@ class ViewsTest(TestCase):
                         'definition': entity_type
                       }
         # must run on an empty kernel db
-        create_new_kernel_object('project', pipeline, project_data)
-        create_new_kernel_object('schema', pipeline, schema_data)
+        utils.create_new_kernel_object('project', pipeline, project_data)
+        utils.create_new_kernel_object('schema', pipeline, schema_data)
         pipeline = Pipeline.objects.get(pk=pipeline.id)
         self.assertIn('PersonX', pipeline.kernel_refs['schema'])
-        create_project_schema_object('Test-Schema-Project', pipeline,
-                                     pipeline.kernel_refs['schema']['PersonX'],
-                                     'PersonX')
+        utils.create_project_schema_object('Test-Schema-Project', pipeline,
+                                           pipeline.kernel_refs['schema']['PersonX'],
+                                           'PersonX')
         self.assertIn('PersonX', pipeline.kernel_refs['projectSchema'])
 
         with self.assertRaises(Exception) as exc:
-            create_new_kernel_object('project', pipeline, {}, 'Aux-test')
+            utils.create_new_kernel_object('project', pipeline, {}, 'Aux-test')
         exception = ast.literal_eval(str(exc.exception))
         self.assertEqual(exception['object_name'], 'unknown')
-        self.assertFalse(is_object_linked(pipeline.kernel_refs, 'schema', 'Person'))
+        self.assertFalse(utils.is_object_linked(pipeline.kernel_refs, 'schema', 'Person'))
