acecda0e861355ebb743a1a71d0bceb6e9e1da5d
obdulia <obdulia.losantos@gmail.com>
2018-06-28 11:10:47 +0200

    feat(kernel): include entities count in stats

--- aether-kernel/aether/kernel/api/serializers.py
+++ aether-kernel/aether/kernel/api/serializers.py
@@ -335,20 +335,23 @@ class EntityLDSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
 class ProjectStatsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     first_submission = serializers.DateTimeField()
     last_submission = serializers.DateTimeField()
-    submission_count = serializers.IntegerField()
+    submissions_count = serializers.IntegerField()
+    entities_count = serializers.IntegerField()
 
     class Meta:
         model = models.Project
         fields = (
             'id', 'name', 'created',
-            'first_submission', 'last_submission', 'submission_count',
+            'first_submission', 'last_submission',
+            'submissions_count', 'entities_count',
         )
 
 
 class MappingStatsSerializer(DynamicFieldsMixin, serializers.ModelSerializer):
     first_submission = serializers.DateTimeField()
     last_submission = serializers.DateTimeField()
-    submission_count = serializers.IntegerField()
+    submissions_count = serializers.IntegerField()
+    entities_count = serializers.IntegerField()
 
     class Meta:
         model = models.Mapping
