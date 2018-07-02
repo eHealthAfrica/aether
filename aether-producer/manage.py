529467f9ca9e76090cc0b17b7acee3c797b7f4f2
shawnsarwar <ssarwar@gmail.com>
2018-07-02 16:42:13 +0200

    chore: added PRODUCER_SETTINGS_FILE to env and updated references (#241)
    * chore: added PRODUCER_SETTINGS_FILE to env and updated references

--- aether-producer/manage.py
+++ aether-producer/manage.py
@@ -24,17 +24,11 @@ from time import sleep
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
-        if sys.argv[1].strip() == "test":
-            print("starting producer for test")
-            from producer import aether_producer
-            aether_producer.main(test=True)
-            print("Started Producer for Test")
-        else:
-            print ("invalid argument: %s" % sys.argv[1])
-    else:
-        print("starting producer")
-        print("waiting for requisites")
-        from producer import aether_producer
-        aether_producer.main(test=False)
-        print("Started Producer")
+    try:
+        settings_path = os.environ['PRODUCER_SETTINGS_FILE']
+    except KeyError:
+        print('PRODUCER_SETTINGS_FILE not set in environment.')
+        sys.exit(1)
+    from producer import aether_producer
+    aether_producer.main(file_path=settings_path)
+    print("Started Producer with path %s" % settings_path)
