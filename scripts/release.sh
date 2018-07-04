5e31c05b0b22125f6c33c1068eaf28eca1ada26b
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-07-04 16:32:53 +0200

    fix: update release process to include aether-producer && build RCs on release branches

--- scripts/release.sh
+++ scripts/release.sh
@@ -30,24 +30,37 @@ docker-compose run   ui-webpack build
 
 # Build docker images
 IMAGE_REPO='ehealthafrica'
-APPS=( kernel odk couchdb-sync ui )
+CORE_APPS=( kernel odk couchdb-sync ui )
+CORE_COMPOSE='docker-compose.yml'
+CONNECT_APPS=( producer )
+CONNECT_COMPOSE='docker-compose-connect.yml'
 VERSION=`cat VERSION`
+
+release_app () {  # ( name of app -> $1, compose_path -> $2 )
+  AETHER_APP="aether-${1}"
+  echo "version: $VERSION"
+  echo "Building Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
+  docker-compose -f $2 build --build-arg GIT_REVISION=$TRAVIS_COMMIT \
+  --build-arg VERSION=$VERSION $1
+
+  docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
+  docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:latest"
+  echo "Pushing Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
+  docker push "${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
+  docker push "${IMAGE_REPO}/${AETHER_APP}:latest"
+}
 
 if [ -z "$TRAVIS_TAG" ];
 then
   VERSION=${VERSION}-rc
 fi
 
-for APP in "${APPS[@]}"
+for APP in "${CORE_APPS[@]}"
 do
-  AETHER_APP="aether-${APP}"
-  echo "version: $VERSION"
-  echo "Building Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
-  docker-compose build --build-arg GIT_REVISION=$TRAVIS_COMMIT \
-  --build-arg VERSION=$VERSION $APP
-  docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
-  docker tag ${AETHER_APP} "${IMAGE_REPO}/${AETHER_APP}:latest"
-  echo "Pushing Docker image ${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
-  docker push "${IMAGE_REPO}/${AETHER_APP}:${VERSION}"
-  docker push "${IMAGE_REPO}/${AETHER_APP}:latest"
+  release_app $APP $CORE_COMPOSE
+done
+
+for CONNECT_APP in "${CONNECT_APPS[@]}"
+do
+  release_app $CONNECT_APP $CONNECT_COMPOSE
 done
