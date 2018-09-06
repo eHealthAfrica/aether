672ef159228673ff1f74ebcf398f1b5dacc5af69
obdulia <obdulia.losantos@gmail.com>
2018-09-06 17:58:45 +0200

    fix: remove entities clean up in tests :(

--- aether-odk-module/aether/odk/api/tests/test_kernel_utils.py
+++ aether-odk-module/aether/odk/api/tests/test_kernel_utils.py
@@ -56,7 +56,6 @@ class KernelUtilsTest(CustomTestCase):
 
         self.KERNEL_HEADERS = get_auth_header()
         self.PROJECT_URL = f'{self.kernel_url}/projects/{str(self.project.project_id)}/'
-        self.ENTITIES_URL = f'{self.kernel_url}/entities/project={str(self.project.project_id)}'
 
         self.MAPPING_URL_1 = f'{self.kernel_url}/mappings/{self.KERNEL_ID_1}/'
         self.SCHEMA_URL_1 = f'{self.kernel_url}/schemas/{self.KERNEL_ID_1}/'
@@ -82,7 +81,6 @@ class KernelUtilsTest(CustomTestCase):
         super(KernelUtilsTest, self).tearDown()
 
         # delete the test objects created in kernel testing server
-        requests.delete(self.ENTITIES_URL, headers=self.KERNEL_HEADERS)
         requests.delete(self.PROJECT_URL, headers=self.KERNEL_HEADERS)
         requests.delete(self.SCHEMA_URL_1, headers=self.KERNEL_HEADERS)
         requests.delete(self.SCHEMA_URL_2, headers=self.KERNEL_HEADERS)
