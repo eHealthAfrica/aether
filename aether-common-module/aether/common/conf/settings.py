f78b4fc2dd8faa34caf80177ec5661b50d5ea079
obdulia <obdulia.losantos@gmail.com>
2018-09-11 10:50:06 +0200

    fix(ui): raise RuntimeError with missing env vars

--- aether-common-module/aether/common/conf/settings.py
+++ aether-common-module/aether/common/conf/settings.py
@@ -246,12 +246,21 @@ DJANGO_STORAGE_BACKEND = os.environ['DJANGO_STORAGE_BACKEND']
 if DJANGO_STORAGE_BACKEND == 'filesystem':
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
     HOSTNAME = os.environ['HOSTNAME']
+    if not HOSTNAME:
+        raise RuntimeError('Missing HOSTNAME')
+
 elif DJANGO_STORAGE_BACKEND == 's3':
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     AWS_STORAGE_BUCKET_NAME = os.environ['BUCKET_NAME']
+    if not AWS_STORAGE_BUCKET_NAME:
+        raise RuntimeError('Missing BUCKET_NAME')
+
 elif DJANGO_STORAGE_BACKEND == 'gcs':
     DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
     GS_BUCKET_NAME = os.environ['BUCKET_NAME']
+    if not GS_BUCKET_NAME:
+        raise RuntimeError('Missing BUCKET_NAME')
+
 else:
     msg = (
         'Unrecognized value "{}" for environment variable DJANGO_STORAGE_BACKEND.'
