3c2d2ce02509a72e214efb0d705c259bb7bcae67
Obdulia Losantos <obdulia.losantos@gmail.com>
2018-09-24 15:53:06 +0200

    feat(common): check url with auth token and check_kernel command (#372)
    * feat(common): set logging level with LOGGING_LEVEL env var
    
    * feat(common): check_url command with auth token
    
    * feat(entrypoint): checks kernel in other containers

--- aether-common-module/aether/common/conf/settings.py
+++ aether-common-module/aether/common/conf/settings.py
@@ -156,13 +156,11 @@ DATABASES = {
 # Logging Configuration
 # ------------------------------------------------------------------------------
 
-LOGGING_LEVEL = logging.INFO
+# https://docs.python.org/3.6/library/logging.html#levels
+LOGGING_LEVEL = os.environ.get('LOGGING_LEVEL', logging.INFO)
 LOGGING_CLASS = 'logging.StreamHandler'
-if DEBUG:
-    LOGGING_LEVEL = logging.DEBUG
 
 if TESTING:
-    LOGGING_LEVEL = logging.CRITICAL
     LOGGING_CLASS = 'logging.NullHandler'
 
 logger = logging.getLogger(__name__)
