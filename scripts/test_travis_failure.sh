40a8fb1a59e62085092b7b11b734f3842115c109
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2018-09-26 14:51:44 +0200

    fix: add dcp log dump to travis after test failure of all or integration tests

---
+++ scripts/test_travis_failure.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2018 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+set -Eeuo pipefail
+
+./scripts/build_common_and_distribute.sh
+
+case "$1" in
+    kubernetes)
+        
+    ;;
+
+    integration)
+        docker-compose -f docker-compose-test.yml logs
+    ;;
+
+    all)
+        docker-compose -f docker-compose-test.yml logs  
+    ;;
+
+esac
