d4651e14e77c84dc3bbb2aed94f60414915d1598
obdulia <obdulia.losantos@gmail.com>
2018-10-10 09:27:36 +0200

    refactor: reformat code

--- aether-kernel/aether/kernel/api/tests/__init__.py
+++ aether-kernel/aether/kernel/api/tests/__init__.py
@@ -24,14 +24,14 @@ SCHEMA_FILE_ERROR = PATH_DIR + 'err_schema.json'
 
 EXAMPLE_MAPPING = {
     'entities': {
-        'Person': '1'
+        'Person': '1',
     },
     'mapping': [
         ['#!uuid', 'Person.id'],
         ['data.village', 'Person.villageID'],
         ['data.people[*].name', 'Person.name'],
-        ['data.people[*].dob', 'Person.dob']
-    ]
+        ['data.people[*].dob', 'Person.dob'],
+    ],
 }
 
 EXAMPLE_SCHEMA = {
@@ -51,7 +51,7 @@ EXAMPLE_SCHEMA = {
             'type': [
                 'null',
                 'string',
-                ],
+            ],
             'name': '_rev',
             'doc': 'REVISION',
             'inherited_from': 'http://ehealthafrica.org/#CouchDoc',
@@ -62,7 +62,7 @@ EXAMPLE_SCHEMA = {
                 'string',
                 {
                     'type': 'array',
-                    'items': 'string'
+                    'items': 'string',
                 }
             ],
             'name': 'name',
@@ -70,7 +70,7 @@ EXAMPLE_SCHEMA = {
         },
         {
             'type': 'string',
-            'name': 'dob'
+            'name': 'dob',
         },
         {
             'jsonldPredicate': {
@@ -80,8 +80,8 @@ EXAMPLE_SCHEMA = {
             'type': 'string',
             'name': 'villageID',
             'doc': 'VILLAGE',
-        }
-    ]
+        },
+    ],
 }
 
 EXAMPLE_SOURCE_DATA = {
@@ -90,18 +90,18 @@ EXAMPLE_SOURCE_DATA = {
         'people': [
             {
                 'name': 'PersonA',
-                'dob': '2000-01-01'
+                'dob': '2000-01-01',
             },
             {
                 'name': 'PersonB',
-                'dob': '2001-01-01'
+                'dob': '2001-01-01',
             },
             {
                 'name': ['FirstC', 'MiddleC', 'LastC'],
-                'dob': '2002-01-01'
-            }
-        ]
-    }
+                'dob': '2002-01-01',
+            },
+        ],
+    },
 }
 
 EXAMPLE_NESTED_SOURCE_DATA = {
@@ -113,44 +113,44 @@ EXAMPLE_NESTED_SOURCE_DATA = {
                 'people': [
                     {
                         'name': 'PersonA',
-                        'dob': '2000-01-01'
+                        'dob': '2000-01-01',
                     },
                     {
                         'name': 'PersonB',
-                        'dob': '2001-01-01'
+                        'dob': '2001-01-01',
                     },
                     {
                         'name': ['FirstC', 'MiddleC', 'LastC'],
-                        'dob': '2002-01-01'
-                    }
-                ]
+                        'dob': '2002-01-01',
+                    },
+                ],
             },
             {
                 'num': 1,
                 'people': [
                     {
                         'name': 'PersonD',
-                        'dob': '2000-01-01'
+                        'dob': '2000-01-01',
                     },
                     {
                         'name': 'PersonE',
-                        'dob': '2001-01-01'
+                        'dob': '2001-01-01',
                     },
                     {
                         'name': 'PersonF',
-                        'dob': '2002-01-01'
-                    }
-                ]
-            }
-        ]
-    }
+                        'dob': '2002-01-01',
+                    },
+                ],
+            },
+        ],
+    },
 }
 
 EXAMPLE_SOURCE_DATA_ENTITY = {
     'villageID': 'somevillageID',
     'name': 'Person-Valid',
     'dob': '2000-01-01',
-    'id': 'somerandomID'
+    'id': 'somerandomID',
 }
 
 EXAMPLE_REQUIREMENTS = {
@@ -159,8 +159,8 @@ EXAMPLE_REQUIREMENTS = {
         '_rev': [],
         'name': ['data.people[*].name'],
         'dob': ['data.people[*].dob'],
-        'villageID': ['data.village']
-    }
+        'villageID': ['data.village'],
+    },
 }
 
 EXAMPLE_ENTITY = {
@@ -169,21 +169,21 @@ EXAMPLE_ENTITY = {
             'id': '1d119b5d-ca71-4f03-a061-1481e1a694ea',
             'name': 'PersonA',
             'dob': '2000-01-01',
-            'villageID': 'somevillageID'
+            'villageID': 'somevillageID',
         },
         {
             'id': '5474b768-92d9-431f-bf90-3c6db1788109',
             'name': 'PersonB',
             'dob': '2001-01-01',
-            'villageID': 'somevillageID'
+            'villageID': 'somevillageID',
         },
         {
             'id': '64d30f72-c15e-4476-9522-d26cb036c73b',
             'name': ['FirstC', 'MiddleC', 'LastC'],
             'dob': '2002-01-01',
-            'villageID': 'somevillageID'
-        }
-    ]
+            'villageID': 'somevillageID',
+        },
+    ],
 }
 
 EXAMPLE_ENTITY_DEFINITION = {'Person': ['id', '_rev', 'name', 'dob', 'villageID']}
@@ -192,7 +192,8 @@ EXAMPLE_FIELD_MAPPINGS = [
     ['#!uuid', 'Person.id'],
     ['data.village', 'Person.villageID'],
     ['data.people[*].name', 'Person.name'],
-    ['data.people[*].dob', 'Person.dob']]
+    ['data.people[*].dob', 'Person.dob'],
+]
 
 SAMPLE_LOCATION_SCHEMA_DEFINITION = {
     'name': 'Location',
@@ -201,24 +202,24 @@ SAMPLE_LOCATION_SCHEMA_DEFINITION = {
         {
             'name': 'id',
             'type': 'string',
-            'jsonldPredicate': '@id'
+            'jsonldPredicate': '@id',
         },
         {
             'name': 'revision',
             'type': [
                 'null',
-                'string'
-            ]
+                'string',
+            ],
         },
         {
             'name': 'lat',
-            'type': 'float'
+            'type': 'float',
         },
         {
             'name': 'lng',
-            'type': 'float'
-        }
-    ]
+            'type': 'float',
+        },
+    ],
 }
 
 SAMPLE_HOUSEHOLD_SCHEMA_DEFINITION = {
@@ -228,126 +229,126 @@ SAMPLE_HOUSEHOLD_SCHEMA_DEFINITION = {
         {
             'name': 'id',
             'type': 'string',
-            'jsonldPredicate': '@id'
+            'jsonldPredicate': '@id',
         },
         {
             'name': 'revision',
             'type': [
                 'null',
-                'string'
-            ]
+                'string',
+            ],
         },
         {
             'name': 'locationID',
             'type': [
                 'null',
-                'string'
+                'string',
             ],
             'jsonldPredicate': {
                 '_id': 'http://ehealthafrica.org/#Location',
-                '_type': '@id'
-            }
-        }
-    ]
+                '_type': '@id',
+            },
+        },
+    ],
 }
 
 SAMPLE_LOCATION_DATA = {
     'lat': 6.951801,
     'lng': -2.7539059999999997,
-    'id': '00f3f1ae-abab-448b-b12f-f9c1839465ab'
+    'id': '00f3f1ae-abab-448b-b12f-f9c1839465ab',
 }
 
 SAMPLE_HOUSEHOLD_DATA = {
-    'locationID': '00f3f1ae-abab-448b-b12f-f9c1839465ab'
+    'locationID': '00f3f1ae-abab-448b-b12f-f9c1839465ab',
 }
 
 EXAMPLE_GAMETOKEN_SCHEMA = {
     'fields': [
-      {
-        'jsonldPredicate': '@id',
-        'type': 'string',
-        'name': 'id',
-        'inherited_from': 'http://game.eha.org/BaseModel'
-      },
-      {
-        'type': 'string',
-        'name': 'rev',
-        'inherited_from': 'http://game.eha.org/BaseModel'
-      },
-      {
-        'doc': 'A description of the thing.',
-        'jsonldPredicate': 'http://game.eha.org/description',
-        'type': [
-          'null',
-          'string',
-          {
-            'items': 'string',
-            'type': 'array'
-          }
-        ],
-        'name': 'description'
-      },
-      {
-        'doc': 'A token value, true for positive, false for negative',
-        'jsonldPredicate': 'http://game.eha.org/tokenValue',
-        'type': [
-          'null',
-          'boolean'
-        ],
-        'name': 'tokenValue'
-      },
-      {
-        'doc': 'The time something was created',
-        'jsonldPredicate': 'http://game.eha.org/generationTime',
-        'type': [
-          'null',
-          'string',
-          {
-            'items': 'string',
-            'type': 'array'
-          }
-        ],
-        'name': 'generationTime'
-      },
-      {
-        'doc': 'A hash to maintain the integrity of generated tokens.',
-        'jsonldPredicate': 'http://game.eha.org/securityHash',
-        'type': [
-          'null',
-          'string',
-          {
-            'items': 'string',
-            'type': 'array'
-          }
-        ],
-        'name': 'securityHash'
-      },
-      {
-        'doc': 'A common name for this entity.',
-        'jsonldPredicate': 'http://game.eha.org/name',
-        'type': [
-          'null',
-          'string',
-          {
-            'items': 'string',
-            'type': 'array'
-          }
-        ],
-        'name': 'name'
-      }
+        {
+            'jsonldPredicate': '@id',
+            'type': 'string',
+            'name': 'id',
+            'inherited_from': 'http://game.eha.org/BaseModel',
+        },
+        {
+            'type': 'string',
+            'name': 'rev',
+            'inherited_from': 'http://game.eha.org/BaseModel',
+        },
+        {
+            'doc': 'A description of the thing.',
+            'jsonldPredicate': 'http://game.eha.org/description',
+            'type': [
+                'null',
+                'string',
+                {
+                    'items': 'string',
+                    'type': 'array',
+                },
+            ],
+            'name': 'description',
+        },
+        {
+            'doc': 'A token value, true for positive, false for negative',
+            'jsonldPredicate': 'http://game.eha.org/tokenValue',
+            'type': [
+                'null',
+                'boolean',
+            ],
+            'name': 'tokenValue',
+        },
+        {
+            'doc': 'The time something was created',
+            'jsonldPredicate': 'http://game.eha.org/generationTime',
+            'type': [
+                'null',
+                'string',
+                {
+                    'items': 'string',
+                    'type': 'array',
+                },
+            ],
+            'name': 'generationTime'
+        },
+        {
+            'doc': 'A hash to maintain the integrity of generated tokens.',
+            'jsonldPredicate': 'http://game.eha.org/securityHash',
+            'type': [
+                'null',
+                'string',
+                {
+                    'items': 'string',
+                    'type': 'array',
+                },
+            ],
+            'name': 'securityHash'
+        },
+        {
+            'doc': 'A common name for this entity.',
+            'jsonldPredicate': 'http://game.eha.org/name',
+            'type': [
+                'null',
+                'string',
+                {
+                    'items': 'string',
+                    'type': 'array',
+                },
+            ],
+            'name': 'name'
+        }
     ],
     'type': 'record',
     'name': 'http://game.eha.org/GameToken',
-    'extends': 'http://game.eha.org/BaseModel'
+    'extends': 'http://game.eha.org/BaseModel',
   }
 
 EXAMPLE_VALID_PAYLOAD = {
     'id': 'bdc639fe-b142-4587-b2e9-4dc1a51f9a5d',
-    'rev': 'some1srevision'
+    'rev': 'some1srevision',
 }
 
 EXAMPLE_INVALID_PAYLOAD = {
     'id': 'bdc639fe-b142-4587-b2e9-4dc1a51f9a5c',
     'rev': 'some1srevision',
-    'tokenValue': 'shouldhavebeenaboolean!'
+    'tokenValue': 'shouldhavebeenaboolean!',
 }
