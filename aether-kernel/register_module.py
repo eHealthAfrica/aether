c09ef328266b1082c029967d66eb127784fb9eb8
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2019-02-07 11:30:18 +0100

    chore: cleanup kernel

--- aether-kernel/register_module.py
+++ aether-kernel/register_module.py
@@ -1,14 +1,35 @@
+# Copyright (C) 2018 by eHealth Africa : http://www.eHealthAfrica.org
+#
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import json
 import requests
 import os
 
 # Kong info
-ENV = lambda x : os.environ.get(x)
+
+
+def ENV(x): return os.environ.get(x)
+
 
 HOST_URL = ENV('BASE_HOST')  # External URL for host
 KONG_URL = f'http://{ENV("KONG_INTERNAL")}/'  # Internal Kong URL
 CLIENT_URL = 'http://kernel:8000/'  # Internal service URL
-CLIENT_NAME = 'kernel' 
+CLIENT_NAME = ENV('APP_ID')
 PLUGIN_URL = f'{KONG_URL}services/{CLIENT_NAME}/plugins'
 ROUTE_URL = f'{KONG_URL}services/{CLIENT_NAME}/routes'
 # Register Client with Kong
@@ -17,13 +38,13 @@ ROUTE_URL = f'{KONG_URL}services/{CLIENT_NAME}/routes'
 print(f'Exposing Service {CLIENT_NAME} @ {CLIENT_URL}')
 
 data = {
-    'name':f'{CLIENT_NAME}',
+    'name': f'{CLIENT_NAME}',
     'url': f'{CLIENT_URL}'
 }
 res = requests.post(
-        f'{KONG_URL}services/',
-        data=data
-    )
+    f'{KONG_URL}services/',
+    data=data
+)
 api_details = res.json()
 api_id = api_details['id']
 
@@ -32,7 +53,7 @@ api_id = api_details['id']
 # # EVERYTHING past / will be JWT controlled
 
 data = {
-    'paths' : [
+    'paths': [
         f'/{CLIENT_NAME}'
     ],
     'strip_path': 'false'
@@ -40,7 +61,7 @@ data = {
 res = requests.post(
     ROUTE_URL,
     data=data
-    )
+)
 
 route_info = res.json()
 protected_route_id = route_info['id']
@@ -48,7 +69,7 @@ protected_route_id = route_info['id']
 # Add a seperate Path for static assets, which we will NOT protect
 
 data = {
-    'paths' : [
+    'paths': [
         f'/{CLIENT_NAME}/static'
     ],
     'strip_path': 'false'
@@ -56,19 +77,19 @@ data = {
 res = requests.post(
     ROUTE_URL,
     data=data
-    )
+)
 
 # Add JWT Plugin to protected route.
 
 ROUTE_URL = f'{KONG_URL}routes/{protected_route_id}/plugins'
 data = {
     'name': 'jwt',
-    'config.cookie_names' : ['aether-jwt']
+    'config.cookie_names': ['aether-jwt']
 }
 res = requests.post(
-        ROUTE_URL,
-        data=data
-    )
+    ROUTE_URL,
+    data=data
+)
 
 # ADD CORS Plugin to Kong for all localhost requests
 
@@ -83,5 +104,5 @@ data = {
 }
 
 res = requests.post(PLUGIN_URL, data=data)
-print(f'Service {CLIENT_NAME} from, {CLIENT_URL}' 
-    + f' now being served by kong @ /{CLIENT_NAME}.')
+print(f'Service {CLIENT_NAME} from, {CLIENT_URL}'
+      + f' now being served by kong @ /{CLIENT_NAME}.')
