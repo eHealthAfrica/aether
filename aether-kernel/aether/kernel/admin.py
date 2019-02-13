aee494508a341efc02b403fd05a5f166b3c0ae70
obdulia <obdulia.losantos@gmail.com>
2019-02-13 11:58:14 +0100

    refactor(kernel): clean up admin

--- aether-kernel/aether/kernel/admin.py
+++ aether-kernel/aether/kernel/admin.py
@@ -22,49 +22,56 @@ from reversion_compare.admin import CompareVersionAdmin
 from .api import models, forms
 
 
-class ProjectAdmin(CompareVersionAdmin):
-    list_display = ('id', 'name', 'revision',)
+class BaseAdmin(CompareVersionAdmin):
+    empty_value_display = '---'
+    list_per_page = 25
     readonly_fields = ('id',)
+    show_full_result_count = True
 
 
-class MappingAdmin(CompareVersionAdmin):
-    form = forms.MappingForm
+class ProjectAdmin(BaseAdmin):
     list_display = ('id', 'name', 'revision',)
-    readonly_fields = ('id',)
 
 
-class MappingSetAdmin(CompareVersionAdmin):
+class MappingSetAdmin(BaseAdmin):
+    form = forms.MappingSetForm
     list_display = ('id', 'name', 'revision', 'project',)
-    readonly_fields = ('id',)
 
 
-class SubmissionAdmin(CompareVersionAdmin):
+class MappingAdmin(BaseAdmin):
+    form = forms.MappingForm
+    list_display = ('id', 'name', 'revision', 'mappingset',)
+
+
+class SubmissionAdmin(BaseAdmin):
     form = forms.SubmissionForm
     list_display = ('id', 'revision', 'mappingset',)
-    readonly_fields = ('id',)
 
 
-class SchemaAdmin(CompareVersionAdmin):
+class AttachmentAdmin(BaseAdmin):
+    list_display = ('id', 'name', 'md5sum', 'submission',)
+    readonly_fields = ('id', 'md5sum',)
+
+
+class SchemaAdmin(BaseAdmin):
     form = forms.SchemaForm
     list_display = ('id', 'name', 'revision',)
-    readonly_fields = ('id',)
 
 
-class ProjectSchemaAdmin(CompareVersionAdmin):
-    list_display = ('id', 'name', 'is_encrypted', 'project', 'schema',)
-    readonly_fields = ('id',)
+class ProjectSchemaAdmin(BaseAdmin):
+    list_display = ('id', 'name', 'revision', 'project', 'schema',)
 
 
-class EntityAdmin(CompareVersionAdmin):
+class EntityAdmin(BaseAdmin):
     form = forms.EntityForm
-    list_display = ('id', 'revision', 'status', 'projectschema',  'submission',)
-    readonly_fields = ('id', 'submission',)
+    list_display = ('id', 'revision', 'status', 'submission', 'mapping',)
 
 
 admin.site.register(models.Project, ProjectAdmin)
 admin.site.register(models.MappingSet, MappingSetAdmin)
 admin.site.register(models.Mapping, MappingAdmin)
 admin.site.register(models.Submission, SubmissionAdmin)
+admin.site.register(models.Attachment, AttachmentAdmin)
 admin.site.register(models.Schema, SchemaAdmin)
 admin.site.register(models.ProjectSchema, ProjectSchemaAdmin)
 admin.site.register(models.Entity, EntityAdmin)
