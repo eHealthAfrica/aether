282c08da5d6af6a9805515c27a51c3a5d7a3904d
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2019-05-15 21:48:49 +0100

    fix: add topic name to kafka facing producer test topic names

--- test-aether-integration-module/test/__init__.py
+++ test-aether-integration-module/test/__init__.py
@@ -32,6 +32,7 @@ from .consumer import get_consumer, read
 FORMS_TO_SUBMIT = 10
 SEED_ENTITIES = 10 * 7  # 7 Vaccines in each report
 SEED_TYPE = 'CurrentStock'
+KAFKA_SEED_TYPE = 'None.CurrentStock'
 
 PRODUCER_CREDS = [
     os.environ['PRODUCER_ADMIN_USER'],
@@ -66,7 +67,7 @@ def wait_for_producer_status():
             kafka = status.get('kafka_container_accessible')
             if not kafka:
                 raise ValueError('Kafka not connected yet')
-            person = status.get('topics', {}).get(SEED_TYPE, {})
+            person = status.get('topics', {}).get(KAFKA_SEED_TYPE, {})
             ok_count = person.get('last_changeset_status', {}).get('succeeded')
             if ok_count:
                 sleep(5)
@@ -109,7 +110,7 @@ def generate_entities(client, mappingset):  # noqa: F811
 
 @pytest.fixture(scope='function')
 def read_people():
-    consumer = get_consumer(SEED_TYPE)
+    consumer = get_consumer(KAFKA_SEED_TYPE)
     messages = read(consumer, start='FIRST', verbose=False, timeout_ms=500)
     consumer.close()  # leaving consumers open can slow down zookeeper, try to stay tidy
     return messages
