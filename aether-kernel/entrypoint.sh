9376c5d246298290280557c499160716df469a49
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2019-05-27 12:20:46 +0200

    fix(sync): start RQ worker along with webserver (#583)
    * fix(sync): start RQ worker along with webserver
    
    * docs: entrypoint help message

--- aether-kernel/entrypoint.sh
+++ aether-kernel/entrypoint.sh
@@ -43,8 +43,8 @@ function show_help {
     test_coverage : run tests with coverage output
     test_py       : alias of test_coverage
 
-    start         : start webserver behind nginx
-    start_dev     : start webserver for development
+    start         : start uWSGI server
+    start_dev     : start Django server for development
 
     health        : checks the system healthy
     """
