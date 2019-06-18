abf42bcb6d5744a64cc39ff2ca51caf526ff1385
Shawn Sarwar <shawn.sarwar@ehealthnigeria.org>
2019-06-18 15:56:54 +0200

    fix: add auth to admin client

--- aether-producer/producer/__init__.py
+++ aether-producer/producer/__init__.py
@@ -101,11 +101,22 @@ class ProducerManager(object):
         # Clear objects and start
         self.kernel = None
         self.kafka = KafkaStatus.SUBMISSION_PENDING
-        self.kafka_admin_client = AdminClient(
-            {"bootstrap.servers": self.settings.get("kafka_url")}
-        )
+        self.get_admin_client()
         self.topic_managers = {}
         self.run()
+
+    def get_admin_client(self):
+        kafka_settings = {"bootstrap.servers": self.settings.get("kafka_url")}
+        if self.settings.get('kafka_security', '').lower() == 'sasl_plaintext':
+            # Let Producer use Kafka SU to produce
+            self.logger.info(f'Using security protocol: SASL_PLAINTEXT')
+            kafka_settings['security.protocol'] = 'SASL_PLAINTEXT'
+            kafka_settings['sasl.mechanisms'] = 'SCRAM-SHA-256'
+            kafka_settings['sasl.username'] = \
+                self.settings.get('KAFKA_SU_USER')
+            kafka_settings['sasl.password'] = \
+                self.settings.get('KAFKA_SU_PW')
+        self.kafka_admin_client = AdminClient(kafka_settings)
 
     def keep_alive_loop(self):
         # Keeps the server up in case all other threads join at the same time.
@@ -457,7 +468,8 @@ class TopicManager(object):
         # check for SASL
         if self.context.settings.get('kafka_security', '').lower() == 'sasl_plaintext':
             # Let Producer use Kafka SU to produce
-            self.kafka_settings['security.protocol'] = 'sasl_plaintext'
+            self.logger.info(f'Using security protocol: SASL_PLAINTEXT')
+            self.kafka_settings['security.protocol'] = 'SASL_PLAINTEXT'
             self.kafka_settings['sasl.mechanisms'] = 'SCRAM-SHA-256'
             self.kafka_settings['sasl.username'] = \
                 self.context.settings.get('KAFKA_SU_USER')
