1ca0d967e0a2459df3e0617eb403f57b620b5ee3
obdulia <obdulia.losantos@gmail.com>
2019-07-11 11:25:46 +0200

    fix: would this work???

--- scripts/kill_all.sh
+++ scripts/kill_all.sh
@@ -23,4 +23,5 @@ set -Eeuo pipefail
 for dc_file in $(find docker-compose*.yml */docker-compose*.yml 2> /dev/null)
 do
     docker-compose -f $dc_file kill
+    docker-compose -f $dc_file down -v
 done
