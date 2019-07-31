dcc380d45e47914460c55d8f185ee87d20e93b1f
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2019-07-31 12:23:59 +0200

    fix(uwsgi): wrong default static url (#639)
    * fix(uwsgi): wrong default static url
    
    * fix: data upload max size

--- aether-kernel/aether/kernel/settings.py
+++ aether-kernel/aether/kernel/settings.py
@@ -49,6 +49,16 @@ REST_FRAMEWORK['DEFAULT_FILTER_BACKENDS'] = [
 ]
 
 
+# Upload files
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/2.2/ref/settings/#std:setting-DATA_UPLOAD_MAX_MEMORY_SIZE
+# https://docs.djangoproject.com/en/2.2/ref/settings/#std:setting-FILE_UPLOAD_MAX_MEMORY_SIZE
+
+_max_size = 10 * 1024 * 1024  # 10MB
+DATA_UPLOAD_MAX_MEMORY_SIZE = int(os.environ.get('DATA_UPLOAD_MAX_MEMORY_SIZE', _max_size))
+FILE_UPLOAD_MAX_MEMORY_SIZE = int(os.environ.get('FILE_UPLOAD_MAX_MEMORY_SIZE', _max_size))
+
+
 # Export Configuration
 # ------------------------------------------------------------------------------
 
