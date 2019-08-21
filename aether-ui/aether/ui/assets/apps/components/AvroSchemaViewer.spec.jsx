27356569a5ca4525248a85c718f6fa9ec4d86b6d
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2019-08-21 17:52:22 +0200

    fix(ui): AVRO Schema viewer (#652)
    * feat: more avro utilities
    
    * fix: simplify AvroSchemaViewer
    
    * fix: missing tests
    
    * fix: enum case
    
    * fix: cleaning
    
    * fix: simplify EntityTypeViewer
    
    * fix: remove EntityTypeViewer
    
    * test: remove entity types mock file
    
    * test: add more cases to mock data
    
    * fix: use extended types
    
    * fix schema viewer styles
    
    * fix: sass-lint
    
    * fix: random bytes and fixed values

--- aether-ui/aether/ui/assets/apps/components/AvroSchemaViewer.spec.jsx
+++ aether-ui/aether/ui/assets/apps/components/AvroSchemaViewer.spec.jsx
@@ -27,20 +27,10 @@ import { mockInputSchema } from '../../tests/mock'
 import { AvroSchemaViewer } from '../components'
 
 describe('AvroSchemaViewer', () => {
-  it('should take a valid avro schema and render an avro visualizer', () => {
-    const component = mountWithIntl(
-      <AvroSchemaViewer schema={mockInputSchema} highlight={{ 'person.forename': 1 }} />
-    )
-
-    expect(component.find('[data-qa^="group-title-"]').length).toEqual(9)
-    expect(component.find('[data-qa="no-children-surname"]').html())
-      .toContain('<span class="name"> surname </span>')
-    expect(component.find('[data-qa="no-children-forename"]').html())
-      .toContain('<span class="name"> forename </span><span class="type">string,int</span>')
-    expect(component.find('[data-qa="no-children-gender"]').html())
-      .toContain('<span class="type"> (nullable)</span>')
-    expect(component.find('[id="input_person.iterate_one.item"]').html())
-      .toContain('<span class="name item"> item </span>')
+  it('should render an empty avro schema viewer', () => {
+    const component = mountWithIntl(<AvroSchemaViewer />)
+    expect(component.text())
+      .toContain('Your AVRO schema will be displayed here once you have added a valid source.')
   })
 
   it('should take a invalid avro schema and render an invalid error message', () => {
@@ -50,12 +40,199 @@ describe('AvroSchemaViewer', () => {
     }
 
     const component = mountWithIntl(<AvroSchemaViewer schema={inputSchemaWithoutName} />)
-    expect(component.html()).toContain('You have provided an invalid AVRO schema.')
+    expect(component.text()).toContain('You have provided an invalid AVRO schema.')
   })
 
-  it('should render an empty avro schema viewer', () => {
-    const component = mountWithIntl(<AvroSchemaViewer />)
-    expect(component.html())
-      .toContain('Your schema for this pipeline will be displayed here once you have added a valid source')
+  it('should take a non-record avro schema and render an invalid error message', () => {
+    const inputSchemaNoRecord = {
+      name: 'id',
+      type: 'string'
+    }
+
+    const component = mountWithIntl(<AvroSchemaViewer schema={inputSchemaNoRecord} />)
+    expect(component.text()).toContain('Initial AVRO schema type can only be of type "record".')
+  })
+
+  it('should take a valid avro schema and highlight the indicated fields', () => {
+    const component = mountWithIntl(
+      <AvroSchemaViewer
+        schema={mockInputSchema}
+        highlight={{ '$.id': 1, '$.dictionary.code': 2 }}
+        pathPrefix='$'
+        className='input-schema'
+      />
+    )
+
+    expect(component.find('[data-qa="$.id"]').html())
+      .toContain('<div class="input-schema-mapped-1 field"><span class="name">id</span>')
+    expect(component.find('[data-qa="$.dictionary.code"]').html())
+      .toContain('<div class="input-schema-mapped-2 field"><span class="name">code</span>')
+  })
+
+  it('should take a valid avro schema and render an avro visualizer', () => {
+    const component = mountWithIntl(
+      <AvroSchemaViewer schema={mockInputSchema} />
+    )
+
+    expect(component.find('[data-qa^="group-title-"]').length).toEqual(1)
+
+    // PRIMITIVES
+
+    const idDiv = component.find('[data-qa="id"]').html()
+    expect(idDiv).toContain('<div class=" field">')
+    expect(idDiv).not.toContain('<i class="fas fa-lock"></i>') // public
+    expect(idDiv).toContain('<span class="name">id</span>')
+    expect(idDiv).toContain('<span class="type">string</span>')
+
+    const textDiv = component.find('[data-qa="text"]').html()
+    expect(textDiv).toContain('<div class=" field">')
+    expect(textDiv).toContain('<span class="name">text</span>')
+    expect(textDiv).toContain('<span class="type">string (nullable)</span>')
+
+    const choicesDiv = component.find('[data-qa="choices"]').html()
+    expect(choicesDiv).toContain('<div class=" field">')
+    expect(choicesDiv).toContain('<span class="name">choices</span>')
+    expect(choicesDiv).toContain('<span class="type">enum (a, b)</span>')
+
+    // RECORDS
+
+    const dictionaryDiv = component.find('[data-qa="dictionary"]').html()
+    expect(dictionaryDiv).toContain('<div data-qa="dictionary" class="group">')
+    expect(dictionaryDiv).toContain('<div class=" group-title">')
+    expect(dictionaryDiv).toContain('<span class="name">dictionary</span>')
+    expect(dictionaryDiv).toContain('<span class="type">record</span>')
+
+    const dictionaryCodeDiv = component.find('[data-qa="dictionary.code"]').html()
+    expect(dictionaryCodeDiv).toContain('<div class=" field">')
+    expect(dictionaryCodeDiv).toContain('<span class="name">code</span>')
+    expect(dictionaryCodeDiv).toContain('<span class="type">int</span>')
+
+    const listNumbersDiv = component.find('[data-qa="list_of_numbers"]').html()
+    expect(listNumbersDiv).toContain('<div class=" field">')
+    expect(listNumbersDiv).toContain('<span class="name">list_of_numbers</span>')
+    expect(listNumbersDiv).toContain('<span class="type">array [union: int, boolean (nullable)]</span>')
+
+    // ARRAYS
+
+    const listTextsDiv = component.find('[data-qa="list_of_texts"]').html()
+    expect(listTextsDiv).toContain('<div class=" field">')
+    expect(listTextsDiv).toContain('<span class="name">list_of_texts</span>')
+    expect(listTextsDiv).toContain('<span class="type">array [string]</span>')
+
+    const listDictionariesDiv = component.find('[data-qa="list_of_dictionaries"]').html()
+    expect(listDictionariesDiv).toContain('<div class=" field">')
+    expect(listDictionariesDiv).toContain('<span class="name">list_of_dictionaries</span>')
+    expect(listDictionariesDiv).toContain('<span class="type">array [record]</span>')
+
+    const listDictionariesWordDiv = component.find('[data-qa="list_of_dictionaries.#.word"]').html()
+    expect(listDictionariesWordDiv).toContain('<div class=" field">')
+    expect(listDictionariesWordDiv).toContain('<span class="name">word</span>')
+    expect(listDictionariesWordDiv).toContain('<span class="type">string</span>')
+
+    const listDictionariesMeaningDiv = component.find('[data-qa="list_of_dictionaries.#.meaning"]').html()
+    expect(listDictionariesMeaningDiv).toContain('<div class=" field">')
+    expect(listDictionariesMeaningDiv).toContain('<span class="name">meaning</span>')
+    expect(listDictionariesMeaningDiv).toContain('<span class="type">string</span>')
+
+    // MAPS
+
+    const mapPrimitivesDiv = component.find('[data-qa="mapping_primitives"]').html()
+    expect(mapPrimitivesDiv).toContain('<div class=" field">')
+    expect(mapPrimitivesDiv).toContain('<span class="name">mapping_primitives</span>')
+    expect(mapPrimitivesDiv).toContain('<span class="type">map {float} (nullable)</span>')
+
+    const mapDictionariesDiv = component.find('[data-qa="mapping_dictionaries"]').html()
+    expect(mapDictionariesDiv).toContain('<div class=" field">')
+    expect(mapDictionariesDiv).toContain('<span class="name">mapping_dictionaries</span>')
+    expect(mapDictionariesDiv).toContain('<span class="type">map {record}</span>')
+
+    const mapDictionariesXDiv = component.find('[data-qa="mapping_dictionaries.#.x"]').html()
+    expect(mapDictionariesXDiv).toContain('<div class=" field">')
+    expect(mapDictionariesXDiv).toContain('<span class="name">x</span>')
+    expect(mapDictionariesXDiv).toContain('<span class="type">double</span>')
+
+    const mapDictionariesYDiv = component.find('[data-qa="mapping_dictionaries.#.y"]').html()
+    expect(mapDictionariesYDiv).toContain('<div class=" field">')
+    expect(mapDictionariesYDiv).toContain('<span class="name">y</span>')
+    expect(mapDictionariesYDiv).toContain('<span class="type">float</span>')
+
+    // UNIONS
+
+    const unionPrimitivesDiv = component.find('[data-qa="primitive_union"]').html()
+    expect(unionPrimitivesDiv).toContain('<div class=" field">')
+    expect(unionPrimitivesDiv).toContain('<span class="name">primitive_union</span>')
+    expect(unionPrimitivesDiv).toContain('<span class="type">union: int, string (nullable)</span>')
+
+    const unionComplexDiv = component.find('[data-qa="complex_union"]').html()
+    expect(unionComplexDiv).toContain('<div class=" group-title">')
+    expect(unionComplexDiv).toContain('<span class="name">complex_union</span>')
+    expect(unionComplexDiv).toContain('<span class="type">union (nullable)</span>')
+
+    const unionComplexBooleanDiv = component.find('[data-qa="complex_union.1"]').html()
+    expect(unionComplexBooleanDiv).toContain('<div class=" field">')
+    expect(unionComplexBooleanDiv).toContain('<span class="name">1</span>')
+    expect(unionComplexBooleanDiv).toContain('<span class="type">boolean</span>')
+
+    const unionComplexStringDiv = component.find('[data-qa="complex_union.2"]').html()
+    expect(unionComplexStringDiv).toContain('<div class=" field">')
+    expect(unionComplexStringDiv).toContain('<span class="name">2</span>')
+    expect(unionComplexStringDiv).toContain('<span class="type">string</span>')
+
+    const unionComplexRecordDiv = component.find('[data-qa="complex_union.3"]').html()
+    expect(unionComplexRecordDiv).toContain('<div class=" group-title">')
+    expect(unionComplexRecordDiv).toContain('<span class="name">3</span>')
+    expect(unionComplexRecordDiv).toContain('<span class="type">record</span>')
+
+    // Extended types and non public
+
+    const locationDiv = component.find('[data-qa="location"]').html()
+    expect(locationDiv).toContain('<div data-qa="location" class="group">')
+    expect(locationDiv).toContain('<div class=" group-title">')
+    expect(locationDiv).toContain('<i class="fas fa-lock"></i>') // restricted
+    expect(locationDiv).toContain('<span class="name">location</span>')
+    expect(locationDiv).toContain('<span class="type">geopoint (nullable)</span>') // not record
+
+    const timestampDiv = component.find('[data-qa="timestamp"]').html()
+    expect(timestampDiv).toContain('<div data-qa="timestamp" class="group">')
+    expect(timestampDiv).toContain('<div class=" field">')
+    expect(timestampDiv).toContain('<span class="name">timestamp</span>')
+    expect(timestampDiv).toContain('<span class="type">dateTime</span>') // not string
+
+    const updatedAtDiv = component.find('[data-qa="updated_at"]').html()
+    expect(updatedAtDiv).toContain('<div data-qa="updated_at" class="group">')
+    expect(updatedAtDiv).toContain('<div class=" field">')
+    expect(updatedAtDiv).toContain('<span class="name">updated_at</span>')
+    expect(updatedAtDiv).toContain('<span class="type">dateTime (nullable)</span>') // not string
+  })
+
+  it('should take a valid avro schema and render an avro visualizer without nested fields', () => {
+    const component = mountWithIntl(
+      <AvroSchemaViewer schema={mockInputSchema} hideChildren />
+    )
+
+    expect(component.find('[data-qa^="group-title-"]').length).toEqual(1)
+
+    // 1st level
+
+    const idDiv = component.find('[data-qa="id"]').html()
+    expect(idDiv).toContain('<div class=" field">')
+    expect(idDiv).toContain('<span class="name">id</span>')
+    expect(idDiv).toContain('<span class="type">string</span>')
+
+    const dictionaryDiv = component.find('[data-qa="dictionary"]').html()
+    expect(dictionaryDiv).toContain('<div data-qa="dictionary" class="group">')
+    expect(dictionaryDiv).toContain('<div class=" field">')
+    expect(dictionaryDiv).toContain('<span class="name">dictionary</span>')
+    expect(dictionaryDiv).toContain('<span class="type">record</span>')
+
+    // 2nd Level
+    expect(component.find('[data-qa="dictionary.code"]')).toEqual({})
+
+    const locationDiv = component.find('[data-qa="location"]').html()
+    expect(locationDiv).toContain('<div data-qa="location" class="group">')
+    expect(locationDiv).toContain('<div class=" field">')
+    expect(locationDiv).toContain('<i class="fas fa-lock"></i>')
+    expect(locationDiv).toContain('<span class="name">location</span>')
+    expect(locationDiv).toContain('<span class="type">geopoint (nullable)</span>')
   })
 })
--- aether-ui/aether/ui/assets/apps/utils/avro-utils.jsx
+++ aether-ui/aether/ui/assets/apps/utils/avro-utils.jsx
@@ -21,11 +21,35 @@
 import avro from 'avsc'
 
 import { generateGUID } from './index'
+import { AVRO_EXTENDED_TYPE } from './constants'
+
+// AVRO types:
+// - primitive: null, boolean, int, long, float, double, bytes, string
+// - complex: record, map, array, union, enum, fixed
+const NULL = 'null'
+const PRIMITIVE_TYPES = [
+  // {"type": "aaa", "name": "a", doc: "b", ...}
+  NULL,
+  'boolean',
+  'int',
+  'long',
+  'float',
+  'double',
+  'bytes',
+  'string',
+
+  // these ones are not primitives but work the same
+  // {"type": {"type": "enum", "name": "e", "symbols": ["A", "B", "C", "D"]}}
+  'enum',
+  // {"type": {"type": "fixed", "size": 16, "name": "f"}}
+  'fixed'
+]
+export const FIELD_ID = 'id'
 
 // In-house workaround to the avsc library to avoid null values
 // in case of union types
 avro.types.UnwrappedUnionType.prototype.random = function () {
-  const types = this.types.filter(({ typeName }) => typeName !== 'null')
+  const types = this.types.filter(({ typeName }) => typeName !== NULL)
   if (types.length === 0) {
     return null
   }
@@ -33,40 +57,138 @@ avro.types.UnwrappedUnionType.prototype.random = function () {
   return types[index].random()
 }
 
+const randomBytes = (len = 8) => Math.floor((1 + Math.random()) * 16 ** len).toString(16).slice(1)
+
+// In-house workaround to the avsc library to avoid buffer serialization
+avro.types.FixedType.prototype.random = function () {
+  return randomBytes(this.size)
+}
+avro.types.BytesType.prototype.random = function () {
+  return randomBytes()
+}
+
 export const parseSchema = (schema) => (
   avro.parse(schema, { noAnonymousTypes: true, wrapUnions: false })
 )
 
-export const isOptionalType = (type) => {
-  return Array.isArray(type) && (type.indexOf('null') > -1)
+export const randomInput = (schema) => {
+  const type = parseSchema(schema)
+  const input = type.random()
+
+  // check if there is a string "id" field
+  if (schema.fields.find(field => field.name === FIELD_ID && field.type === 'string')) {
+    input[FIELD_ID] = generateGUID() // make it more UUID
+  }
+  return input
 }
 
-export const makeOptionalType = (type) => {
-  if (isOptionalType(type)) {
-    return type
-  }
-  if (Array.isArray(type)) {
-    return ['null', ...type]
-  }
-  return ['null', type]
-}
+/**
+ * Indicates if the given AVRO type corresponds to a "nullable" type
+ *
+ * @param {*} type   - The AVRO type
+ *
+ * @return {boolean} - true if type is a union type and one of the options is "null"
+ *                     Otherwise false
+ */
+export const isOptionalType = (type) => (
+  Array.isArray(type) &&
+  type.filter(v => (v.type || v) === NULL).length > 0 &&
+  type.filter(v => (v.type || v) !== NULL).length > 0
+)
 
-export const makeOptionalField = (field) => {
-  // The top-level "id" field is reserved for unique ids; do not make it
-  // optional.
-  if (field.name === 'id') { return field }
-  return { ...field, type: makeOptionalType(field.type) }
+/**
+ * Converts the given AVRO type into a "nullable" type
+ *
+ * @param {*} type   - The AVRO type
+ *
+ * @return {array}   - Array of AVRO types, one of the entries is "null".
+ */
+export const makeOptionalType = (type) => (
+  isOptionalType(type) ? type : [NULL, ...(Array.isArray(type) ? type : [type])]
+)
+
+/**
+ * Converts the given AVRO field into a "nullable" field
+ *
+ * @param {object} field   - The AVRO field
+ *
+ * @return {object}        - The same AVRO field but its "type" is an array of
+ *                           AVRO types, one of the entries is "null".
+ */
+export const makeOptionalField = (field) => (
+  // The top-level "id" field is reserved for unique ids; do not make it optional.
+  (field.name === FIELD_ID) ? field : { ...field, type: makeOptionalType(field.type) }
+)
+
+/**
+ * Indicates if the given AVRO type corresponds to a "primitive" type
+ *
+ * @param {*} type   - The AVRO type
+ *
+ * @return {boolean} - true if type is
+ *                       - primitive or
+ *                       - array of primitives or
+ *                       - "nullable" primitive
+ *                     Otherwise false
+ *                       - record
+ *                       - map
+ *                       - tagged union
+ */
+export const isPrimitive = (type) => (
+  // Real primitives: {"type": "aaa"}
+  PRIMITIVE_TYPES.indexOf(type) > -1 ||
+  // Complex types but taken as primitives: {"type": {"type": "zzz"}}
+  (type.type && isPrimitive(type.type)) ||
+  // array of primitives
+  (type.type === 'array' && isPrimitive(type.items)) ||
+  // union of primitives
+  (Array.isArray(type) && type.filter(isPrimitive).length === type.length)
+)
+
+export const typeToString = (type, nullable = '(nullable)', short = false, extended = null) => {
+  const flat = (a) => Array.isArray(a) && a.length === 1 ? a[0] : a
+  const clean = (type) => flat(
+    !isOptionalType(type) ? type : type.filter(v => (v.type || v) !== NULL)
+  )
+
+  const suffix = isOptionalType(type) ? ' ' + nullable : ''
+  const cleanType = clean(type)
+  const t = flat((cleanType && (cleanType[AVRO_EXTENDED_TYPE] || cleanType.type)) || cleanType)
+
+  const typeStr = Array.isArray(t) ? 'union' : extended || t
+  let childrenStr = ''
+  switch (t) {
+    case 'enum':
+      childrenStr = ` (${cleanType.symbols.join(', ')})`
+      break
+
+    case 'map':
+      childrenStr = ` {${typeToString(cleanType.values, nullable, short, cleanType[AVRO_EXTENDED_TYPE])}}`
+      break
+
+    case 'array':
+      childrenStr = ` [${typeToString(cleanType.items, nullable, short, cleanType[AVRO_EXTENDED_TYPE])}]`
+      break
+
+    default:
+      if (Array.isArray(t)) {
+        childrenStr = !short || isPrimitive(t)
+          ? ': ' + t.map(v => typeToString(v, nullable, short)).join(', ')
+          : ''
+      }
+  }
+  return `${typeStr}${childrenStr}${suffix}`
 }
 
 export const deriveEntityTypes = (schema, schemaName = null) => {
   const fields = schema.fields.map(makeOptionalField)
-  if (!fields.find(field => field.name === 'id')) {
+  if (!fields.find(field => field.name === FIELD_ID)) {
     // DETECTED CONFLICT
     // the "id" must be an string if the schema defines it with another type
     // the validation could fail
     // this step only includes it if missing but does not change its type to "string"
     fields.push({
-      name: 'id',
+      name: FIELD_ID,
       type: 'string'
     })
   }
@@ -83,11 +205,11 @@ export const deriveMappingRules = (schema, schemaName = null) => {
   }
   const rules = schema.fields.map(fieldToMappingRule)
 
-  if (!schema.fields.find(field => field.name === 'id')) {
+  if (!schema.fields.find(field => field.name === FIELD_ID)) {
     rules.push({
       id: generateGUID(),
       source: `#!uuid`,
-      destination: `${schemaName || schema.name}.id`
+      destination: `${schemaName || schema.name}.${FIELD_ID}`
     })
   }
   return rules
