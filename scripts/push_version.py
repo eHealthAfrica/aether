612a376b1df6a4e294138deac31786e064b811b7
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2019-10-21 13:57:53 +0200

    chore(travis): push to google cloud storage (#711)

--- scripts/push_version.py
+++ scripts/push_version.py
@@ -21,6 +21,7 @@
 
 import argparse
 import os
+import sys
 
 from google.cloud import exceptions
 from google.cloud import storage
@@ -30,27 +31,43 @@ parser.add_argument('--projects', required=True)
 parser.add_argument('--version', required=True)
 
 
+def get_required(name):
+    try:
+        return os.environ[name]
+    except KeyError as key:
+        raise RuntimeError(f'Missing {key} environment variable!')
+
+
 def setup():
     client = storage.Client()
-    bucket_name = os.getenv('RELEASE_BUCKET')
-    client.from_service_account_json(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-    bucket = client.get_bucket(bucket_name)
+    client.from_service_account_json(GCS_CREDS)
+    bucket = client.get_bucket(GCS_BUCKET)
     if not bucket.exists():
-        raise ValueError('Bucket: {} does not exist or \
-                          is not accessible.'.format(bucket_name))
+        raise RuntimeError(f'Bucket: {GCS_BUCKET} does not exist or is not accessible.')
     return bucket
 
 
-def push_release():
+def push_release(version, projects):
     bucket = setup()
-    args = parser.parse_args()
-    for project in args.projects.split(','):
+    for project in projects:
+        print(f'Pushing new version "{version}" for project "{project}"...')
         try:
-            blob = bucket.blob('{}/version'.format(project))
-            blob.upload_from_string(args.version, content_type='text/plain')
+            blob = bucket.blob(f'{project}/version')
+            blob.upload_from_string(version, content_type='text/plain')
         except exceptions.GoogleCloudError as err:
-            raise ValueError(err)
+            raise RuntimeError(err)
 
 
 if __name__ == '__main__':
-    push_release()
+    try:
+        GCS_BUCKET = get_required('RELEASE_BUCKET')
+        GCS_CREDS = get_required('GOOGLE_APPLICATION_CREDENTIALS')
+
+        args = parser.parse_args()
+        projects = args.projects.split(',')
+        version = args.version
+
+        push_release(version, projects)
+    except Exception as e:
+        print(str(e), flush=True)
+        sys.exit(1)
