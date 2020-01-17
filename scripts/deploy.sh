73e2922d9ac0c11fd6d47f70ad836862d39842e9
obdulia <obdulia.losantos@gmail.com>
2020-01-17 10:11:46 +0100

    fix: last tweaks

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -21,61 +21,42 @@
 
 set -Eo pipefail
 
-function pull_images {
-    for APP in "${DEPLOY_APPS[@]}"; do
-        AETHER_APP="aether-${APP}"
-        SRC_IMG="${DOCKER_IMAGE_REPO}/${AETHER_APP}:${DOCKER_VERSION}"
-
-        echo "Pulling Docker image ${SRC_IMG}"
-        docker pull "${SRC_IMG}"
-        echo "${LINE}"
-    done
-}
-
-function push_images {
-    for APP in "${DEPLOY_APPS[@]}"; do
-        AETHER_APP="aether-${APP}"
-
-        SRC_IMG="${DOCKER_IMAGE_REPO}/${AETHER_APP}:${DOCKER_VERSION}"
-        DEST_IMG="${GCR_IMAGE_REPO}/${AETHER_APP}:${GCS_VERSION}"
-
-        echo "Pushing GCS image ${DEST_IMG}"
-        docker tag "$SRC_IMG" "$DEST_IMG"
-        docker push "${DEST_IMG}"
-        echo "${LINE}"
-    done
-}
-
 LINE=`printf -v row "%${COLUMNS:-$(tput cols)}s"; echo ${row// /#}`
 
 if [[ ${TRAVIS_TAG} =~ ^[0-9]+(\.[0-9]+){2}$ ]]; then
 
     echo "${LINE}"
     echo "Skipping production deployment (temporary)"
+    echo "${LINE}"
     exit 0
 
-    # DOCKER_VERSION=${TRAVIS_TAG}
-    # GCS_VERSION=${TRAVIS_TAG}
-    # GCS_PROJECTS="eha-data"
+    DOCKER_VERSION=${TRAVIS_TAG}
+    GCR_VERSION=${TRAVIS_TAG}
+    GCS_PROJECTS="eha-data"
+    GCR_PROJECT="production-228613"
 
 elif [[ ${TRAVIS_BRANCH} =~ ^release\-[0-9]+\.[0-9]+$ ]]; then
 
     echo "${LINE}"
-    echo "Skipping release candidates deployment"
+    echo "Skipping release candidates deployment (temporary)"
+    echo "${LINE}"
     exit 0
 
-    # FILE_VERSION=`cat VERSION`
-    # DOCKER_VERSION="${FILE_VERSION}-rc"
+    FILE_VERSION=`cat VERSION`
+    DOCKER_VERSION="${FILE_VERSION}-rc"
 
-    # GCS_VERSION="${DOCKER_VERSION}-${TRAVIS_COMMIT}"
-    # # deploy release candidates in ???
-    # GCS_PROJECTS="alpha"
+    GCR_VERSION="${DOCKER_VERSION}-${TRAVIS_COMMIT}"
+
+    # deploy release candidates in ???
+    GCS_PROJECTS="alpha"
+    GCR_PROJECT="development-223016"
 
 else
 
     DOCKER_VERSION="alpha"
-    GCS_VERSION=${TRAVIS_COMMIT}
+    GCR_VERSION=${TRAVIS_COMMIT}
     GCS_PROJECTS="alpha"
+    GCR_PROJECT="development-223016"
 
 fi
 
@@ -87,15 +68,16 @@ export RELEASE_BUCKET="aether-releases"
 export GOOGLE_APPLICATION_CREDENTIALS="gcs_key.json"
 
 echo "${LINE}"
-echo "Docker images tag:   $DOCKER_VERSION"
-echo "Deployment version:  $GCS_VERSION"
-echo "Deployment project:  $GCS_PROJECTS"
-echo "Images:              ${DEPLOY_APPS[@]}"
+echo "Docker images:        ${DEPLOY_APPS[@]}"
+echo "Docker images tag:    $DOCKER_VERSION"
+echo "Deployment version:   $GCR_VERSION"
+echo "Deployment project:   $GCR_PROJECT"
+echo "Kubernetes projects:  $GCS_PROJECTS"
 echo "${LINE}"
 
 
 # ===========================================================
-# install dependencies and create GCS credentials files
+# install dependencies and create GC credentials files
 openssl aes-256-cbc \
     -K $encrypted_9112fb2807d4_key \
     -iv $encrypted_9112fb2807d4_iv \
@@ -105,39 +87,46 @@ openssl aes-256-cbc \
 
 pip install -q google-cloud-storage push-app-version
 
+
 # ===========================================================
 # pull images from public docker hub
 
-# Note: pulling only the alpha images (tags are already in docker hub)
-if [[ $TRAVIS_BRANCH = "develop" ]]; then
-    pull_images
-fi
+for APP in "${DEPLOY_APPS[@]}"; do
+    AETHER_APP="aether-${APP}"
+    SRC_IMG="${DOCKER_IMAGE_REPO}/${AETHER_APP}:${DOCKER_VERSION}"
+
+    echo "Pulling Docker image ${SRC_IMG}"
+    docker pull "${SRC_IMG}"
+    echo "${LINE}"
+done
+
 
 # ===========================================================
 # push images to deployment repository
 
-if
+GCR_REPO_URL="https://eu.gcr.io"
+GCR_IMAGE_REPO="eu.gcr.io/${GCR_PROJECT}/aether"
 
-### GCR_URL="https://eu.gcr.io"
-GCR_REPO_URL="http://eu.gcr.io"
-GCR_IMAGE_REPO="eu.gcr.io/development-223016/aether"
+docker login -u _json_key -p "$(cat gcs_key.json)" $GCR_REPO_URL
 
-### docker login -u _json_key -p "$(cat gcs_key.json)" ${GCR_URL}
+for APP in "${DEPLOY_APPS[@]}"; do
+    AETHER_APP="aether-${APP}"
 
-# Note: pushing only the alpha commit (tags are already in docker hub)
-if [[ $TRAVIS_BRANCH = "develop" ]]; then
-    IMAGE_REPO="eu.gcr.io"
-    PROJECT="development-223016"
-    docker login -u _json_key -p "$(cat gcs_key.json)" $GCR_REPO_URL
+    SRC_IMG="${DOCKER_IMAGE_REPO}/${AETHER_APP}:${DOCKER_VERSION}"
+    DEST_IMG="${GCR_IMAGE_REPO}/${AETHER_APP}:${GCR_VERSION}"
 
-    push_images
-fi
+    echo "Pushing GCR image ${DEST_IMG}"
+    docker tag "$SRC_IMG" "$DEST_IMG"
+    docker push "${DEST_IMG}"
+    echo "${LINE}"
+done
 
-### docker logout ${GCR_URL} || true
+docker logout ${GCR_REPO_URL} || true
+
 
 # ===========================================================
 # notify to Google Cloud Storage the new images
 
 python ./scripts/push_version.py \
-    --version $GCS_VERSION \
+    --version $GCR_VERSION \
     --projects $GCS_PROJECTS
