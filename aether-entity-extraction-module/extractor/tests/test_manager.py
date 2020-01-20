721a97044b892cde01251b4da00c63df96543d16
obdulia <obdulia.losantos@gmail.com>
2020-01-20 11:40:01 +0100

    test: cleaning and more tests

--- aether-entity-extraction-module/extractor/tests/test_manager.py
+++ aether-entity-extraction-module/extractor/tests/test_manager.py
@@ -20,8 +20,9 @@ import collections
 import json
 import uuid
 import copy
-import requests
+
 from unittest import TestCase, mock
+
 from ..manager import ExtractionManager
 from . import MAPPINGS, MAPPINGSET, TENANT, SCHEMA_DECORATORS, SCHEMAS, SUBMISSION
 from ..utils import KERNEL_ARTEFACT_NAMES, Task
@@ -31,21 +32,21 @@ import fakeredis
 SUBMISSION_CHANNEL = 'test_submissions'
 
 
-def build_key(_type, tenant, id):
+def build_redis_key(_type, tenant, id):
     return f'_{_type}:{tenant}:{id}'
 
 
 def load_redis(redis):
     # load mappingset
     redis.set(
-        build_key(KERNEL_ARTEFACT_NAMES.mappingsets, TENANT, MAPPINGSET['id']),
+        build_redis_key(KERNEL_ARTEFACT_NAMES.mappingsets, TENANT, MAPPINGSET['id']),
         json.dumps(MAPPINGSET)
     )
 
     # load mappings
     [
         redis.set(
-            build_key(KERNEL_ARTEFACT_NAMES.mappings, TENANT, m['id']),
+            build_redis_key(KERNEL_ARTEFACT_NAMES.mappings, TENANT, m['id']),
             json.dumps(m)
         )
         for m in MAPPINGS
@@ -54,7 +55,7 @@ def load_redis(redis):
     # load schemas
     [
         redis.set(
-            build_key(KERNEL_ARTEFACT_NAMES.schemas, TENANT, s['id']),
+            build_redis_key(KERNEL_ARTEFACT_NAMES.schemas, TENANT, s['id']),
             json.dumps(s)
         )
         for s in SCHEMAS
@@ -63,7 +64,7 @@ def load_redis(redis):
     # load schema decorators
     [
         redis.set(
-            build_key(KERNEL_ARTEFACT_NAMES.schemadecorators, TENANT, sd['id']),
+            build_redis_key(KERNEL_ARTEFACT_NAMES.schemadecorators, TENANT, sd['id']),
             json.dumps(sd)
         )
         for sd in SCHEMA_DECORATORS
@@ -72,37 +73,42 @@ def load_redis(redis):
 
 class ExtractionManagerTests(TestCase):
 
-    redis = fakeredis.FakeStrictRedis()
-    NO_OF_SUBMISSIONS = 10
-    data = SUBMISSION
-    data['id'] = str(uuid.uuid4())
-    test_task = Task(
-        id=data['id'],
-        data=data,
-        type=f'_{SUBMISSION_CHANNEL}',
-        tenant=TENANT
-    )
+    def setUp(self):
+        super(ExtractionManagerTests, self).setUp()
+
+        data = copy.deepcopy(SUBMISSION)
+        data['id'] = str(uuid.uuid4())
+
+        self.test_task = Task(
+            id=data['id'],
+            data=data,
+            type=f'_{SUBMISSION_CHANNEL}',
+            tenant=TENANT,
+        )
+
+        self.redis = fakeredis.FakeStrictRedis()
+        load_redis(self.redis)
+
+        self.manager = ExtractionManager(self.redis)
+        self.assertIsNotNone(self.manager.redis, None)
 
     def test_init_extraction_manager(self):
         manager = ExtractionManager()
+        self.assertIsNone(manager.redis)
         self.assertEqual(manager.SUBMISSION_QUEUE, collections.deque())
         self.assertEqual(manager.PROCESSED_SUBMISSIONS, collections.deque())
         self.assertEqual(manager.realm_entities, {})
         self.assertFalse(manager.is_extracting)
         self.assertFalse(manager.is_pushing_to_kernel)
-        self.assertIsNone(manager.redis)
 
     def test_handle_pending_submissions(self):
-        load_redis(self.redis)
         self.assertEqual(len(self.redis.execute_command('keys', '*')), 9)
 
-        manager = ExtractionManager(self.redis)
-        self.assertIsNotNone(manager.redis, None)
-
         # make submissions
-        for x in range(self.NO_OF_SUBMISSIONS):
+        NO_OF_SUBMISSIONS = 10
+        for x in range(NO_OF_SUBMISSIONS):
             SUBMISSION['id'] = str(uuid.uuid4())
-            key = build_key(SUBMISSION_CHANNEL, TENANT, SUBMISSION['id'])
+            key = build_redis_key(SUBMISSION_CHANNEL, TENANT, SUBMISSION['id'])
             publish_key = f'__keyspace@0__:{key}'
             data = json.dumps(SUBMISSION)
             self.redis.set(key, data)
@@ -110,59 +116,95 @@ class ExtractionManagerTests(TestCase):
 
         self.assertEqual(
             len(self.redis.execute_command('keys', f'_{SUBMISSION_CHANNEL}*')),
-            self.NO_OF_SUBMISSIONS
+            NO_OF_SUBMISSIONS
         )
-        self.assertEqual(len(manager.SUBMISSION_QUEUE), 0)
+        self.assertEqual(len(self.manager.SUBMISSION_QUEUE), 0)
 
-        manager.handle_pending_submissions(f'_{SUBMISSION_CHANNEL}*')
-        self.assertNotEqual(len(manager.SUBMISSION_QUEUE), 0)
+        self.manager.handle_pending_submissions(f'_{SUBMISSION_CHANNEL}*')
+        self.assertNotEqual(len(self.manager.SUBMISSION_QUEUE), 0)
 
     def test_add_to_queue(self):
-        manager = ExtractionManager(self.redis)
-        manager.add_to_queue(None)
-        self.assertFalse(manager.is_extracting)
-        self.assertIsNone(manager.push_to_kernel_thread)
-        self.assertIsNone(manager.extraction_thread)
+        self.manager.add_to_queue(None)
+        self.assertFalse(self.manager.is_extracting)
+        self.assertIsNone(self.manager.push_to_kernel_thread)
+        self.assertIsNone(self.manager.extraction_thread)
 
-        manager.add_to_queue(self.test_task)
-        self.assertTrue(manager.is_extracting)
-        self.assertIsNotNone(manager.push_to_kernel_thread)
-        self.assertIsNotNone(manager.extraction_thread)
-        self.assertTrue(manager.push_to_kernel_thread.is_alive())
-        self.assertTrue(manager.extraction_thread.is_alive())
+        self.manager.add_to_queue(self.test_task)
+        self.assertTrue(self.manager.is_extracting)
+        self.assertIsNotNone(self.manager.push_to_kernel_thread)
+        self.assertIsNotNone(self.manager.extraction_thread)
+        self.assertTrue(self.manager.push_to_kernel_thread.is_alive())
+        self.assertTrue(self.manager.extraction_thread.is_alive())
 
     def test_entity_extraction(self):
-        load_redis(self.redis)
-        manager = ExtractionManager(self.redis)
-        self.assertEqual(len(manager.PROCESSED_SUBMISSIONS), 0)
-        self.assertEqual(manager.realm_entities, {})
+        self.assertEqual(len(self.manager.PROCESSED_SUBMISSIONS), 0)
+        self.assertEqual(self.manager.realm_entities, {})
 
         # test extraction with missing schema definition in schema decorator
         remove_definitions = copy.deepcopy(SCHEMA_DECORATORS)
         for sd in remove_definitions:
             sd.pop(KERNEL_ARTEFACT_NAMES.schema_definition)
             self.redis.set(
-                build_key(KERNEL_ARTEFACT_NAMES.schemadecorators, TENANT, sd['id']),
+                build_redis_key(KERNEL_ARTEFACT_NAMES.schemadecorators, TENANT, sd['id']),
                 json.dumps(sd)
             )
-        manager.entity_extraction(self.test_task)
-        self.assertEqual(len(manager.PROCESSED_SUBMISSIONS), 1)
-        self.assertEqual(len(manager.realm_entities[TENANT]), 3)
 
-    @mock.patch(
-        'extractor.utils.kernel_data_request'
-    )
-    def test_push_to_kernel(self, mock_kernel_data_request):
-        mock_response = requests.Response()
-        mock_response.status_code = 500
-        mock_kernel_data_request.return_value = mock_response
-        manager = ExtractionManager()
-        manager.realm_entities[TENANT] = collections.deque()
-        manager.realm_entities[TENANT].appendleft({'name': 'test entity', 'submission': 'id_1'})
-        manager.PROCESSED_SUBMISSIONS.appendleft({
+        self.manager.entity_extraction(self.test_task)
+        self.assertEqual(len(self.manager.PROCESSED_SUBMISSIONS), 1)
+        self.assertEqual(len(self.manager.realm_entities[TENANT]), 3)
+
+    @mock.patch('extractor.manager.kernel_data_request', side_effect=Exception)
+    def test_push_to_kernel__error(self, mock_kernel_data_request):
+        self.manager.realm_entities[TENANT] = collections.deque()
+        self.manager.realm_entities[TENANT].appendleft({'name': 'test entity', 'submission': 'id_1'})
+        self.manager.PROCESSED_SUBMISSIONS.appendleft({
             'name': 'test submission',
             'tenant': TENANT,
             'id': 'id_1',
             'mappings': ['1', '2']
         })
-        manager.push_to_kernel()
+
+        self.manager.push_to_kernel()
+
+        mock_kernel_data_request.assert_has_calls([
+            mock.call(
+                url='entities/',
+                method='post',
+                data=[{'name': 'test entity', 'submission': 'id_1'}],
+                realm=TENANT,
+            ),
+            mock.call(
+                url='submissions/bulk_update_extracted/',
+                method='patch',
+                data=[{'name': 'test submission', 'id': 'id_1'}],
+            ),
+        ])
+
+    @mock.patch('extractor.manager.kernel_data_request', return_value={})
+    @mock.patch('extractor.manager.cache_failed_entities')
+    def test_push_to_kernel(self, mock_cache_failed_entities, mock_kernel_data_request):
+        self.manager.realm_entities[TENANT] = collections.deque()
+        self.manager.realm_entities[TENANT].appendleft({'name': 'test entity', 'submission': 'id_1'})
+        self.manager.PROCESSED_SUBMISSIONS.appendleft({
+            'name': 'test submission',
+            'tenant': TENANT,
+            'id': 'id_1',
+            'mappings': ['1', '2']
+        })
+
+        self.manager.push_to_kernel()
+
+        mock_kernel_data_request.assert_has_calls([
+            mock.call(
+                url='entities/',
+                method='post',
+                data=[{'name': 'test entity', 'submission': 'id_1'}],
+                realm=TENANT,
+            ),
+            mock.call(
+                url='submissions/bulk_update_extracted/',
+                method='patch',
+                data=[{'name': 'test submission', 'id': 'id_1'}],
+            ),
+        ])
+        mock_cache_failed_entities.assert_not_called()
