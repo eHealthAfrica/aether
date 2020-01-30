d42af83a055958c2a8fb8f6e77df7f226da9fb00
obdulia <obdulia.losantos@gmail.com>
2020-01-30 16:04:56 +0100

    fix: last tweak

--- aether-kernel/aether/kernel/api/exporter.py
+++ aether-kernel/aether/kernel/api/exporter.py
@@ -128,19 +128,18 @@ class ExporterMixin():
                 return value
 
         json_filter = f'{self.json_field}__'
+        data = self.request.data if isinstance(self.request.data, dict) else {}
         filters = [
             # GET method: query params
             (k, v)
             for k, v in self.request.query_params.items()
             if k.startswith(json_filter)
+        ] + [
+            # POST method: data content
+            (k, v)
+            for k, v in data.items()
+            if k.startswith(json_filter)
         ]
-        if isinstance(self.request.data, dict):
-            filters += [
-                # POST method: data content
-                (k, v)
-                for k, v in self.request.data.items()
-                if k.startswith(json_filter)
-            ]
         queryset = self.queryset
         for k, v in filters:
             kwargs = {k: parse_value(v)}
