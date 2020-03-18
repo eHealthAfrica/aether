705cf3cb7c2217c383f33a20604e1bb5758b48f7
obdulia <obdulia.losantos@gmail.com>
2020-03-18 12:18:38 +0100

    refactor: simplify Modal component

--- aether-ui/aether/ui/assets/apps/components/Modal.jsx
+++ aether-ui/aether/ui/assets/apps/components/Modal.jsx
@@ -18,15 +18,50 @@
  * under the License.
  */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
-import Portal from './Portal'
-import ModalDialog from './ModalDialog'
+// https://reactjs.org/docs/portals.html
 
-const Modal = ({ onEnter, onEscape, ...props }) => (
+const Portal = ({ children, onEscape, onEnter }) => {
+  const [element] = useState(document.createElement('div'))
+
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      if (event.key === 'Escape' && onEscape) {
+        onEscape(event)
+      }
+      if (event.key === 'Enter' && onEnter) {
+        onEnter(event)
+      }
+    }
+
+    document.body.appendChild(element)
+    document.addEventListener('keydown', onKeyDown)
+
+    return () => {
+      document.body.removeChild(element)
+      document.removeEventListener('keydown', onKeyDown)
+    }
+  })
+
+  return ReactDOM.createPortal(children, element)
+}
+
+const Modal = ({ onEnter, onEscape, header, children, buttons }) => (
   <Portal onEnter={onEnter} onEscape={onEscape}>
     <div className='modal show'>
-      <ModalDialog {...props} />
+      <div className='modal-dialog'>
+        <div className='modal-header'>
+          <span className='modal-title'>{header}</span>
+        </div>
+
+        <div className='modal-content'>
+          {children}
+
+          <div className='modal-actions'>{buttons}</div>
+        </div>
+      </div>
     </div>
   </Portal>
 )
