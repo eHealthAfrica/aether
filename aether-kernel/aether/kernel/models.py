15f65ecf74fc5adca914f40324f134e4aa3515c0
Obdulia Losantos <obdulia.losantos@ehealthnigeria.org>
2021-06-03 15:22:12 +0200

    fix(producer): create topics for active projects (#947)
    * chore: upgrade kafka containers
    
    * fix(producer): create topics for active projects
    
    * fix: reduce retention to 1 hour
    
    * fix: added filter tests

--- aether-kernel/aether/kernel/models.py
+++ aether-kernel/aether/kernel/models.py
@@ -64,8 +64,11 @@ _CREATE_SCHEMA_VIEW = '''
         FROM kernel_schemadecorator           AS sd
         INNER JOIN kernel_schema              AS s
                 ON sd.schema_id = s.id
+        INNER JOIN kernel_project             AS p
+                ON sd.project_id = p.id
+               AND p.active IS TRUE
         INNER JOIN multitenancy_mtinstance_vw AS mt
-                ON sd.project_id = mt.instance_id
+                ON p.id = mt.instance_id
         ORDER BY 1 ASC
     ;
 '''
@@ -87,6 +90,9 @@ _CREATE_ENTITY_VIEW = '''
             s.realm
 
         FROM kernel_entity          AS e
+        INNER JOIN kernel_mapping   AS m
+                ON e.mapping_id = m.id
+               AND m.is_active IS TRUE
         INNER JOIN kernel_schema_vw AS s
                 ON e.schemadecorator_id = s.schemadecorator_id
         ORDER BY e.modified ASC
